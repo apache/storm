ab4749798fb445798976330c7c9991aec47796fd
andy_lock_farm <lockfarmuk@gmail.com>
2013-06-18 11:02:38 +0100

    Initial commit

---
+++ src/main/java/backtype/storm/contrib/jms/JmsBatch.java
@@ -0,0 +1,10 @@
+package backtype.storm.contrib.jms;
+
+/**
+ * Batch coordination metadata object for the TridentJmsSpout.
+ * This implementation does not use batch metadata, so the object is empty.
+ *
+ */
+public class JmsBatch {
+    // Empty class
+}
