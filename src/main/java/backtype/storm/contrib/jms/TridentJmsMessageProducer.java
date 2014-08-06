a9e7339b7450f036ee60020d61d52f213b8ed00f
Parth Brahmbhatt <brahmbhatt.parth@gmail.com>
2014-08-06 09:37:52 -0700

    Jms trident state that writes messages to jms as part of trident topology.

---
+++ src/main/java/backtype/storm/contrib/jms/TridentJmsMessageProducer.java
@@ -0,0 +1,23 @@
+package backtype.storm.contrib.jms;
+
+import backtype.storm.tuple.Tuple;
+import storm.trident.tuple.TridentTuple;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import java.io.Serializable;
+
+public interface TridentJmsMessageProducer extends Serializable{
+
+	/**
+	 * Translate a <code>backtype.storm.tuple.TridentTuple</code> object
+	 * to a <code>javax.jms.Message</code object.
+	 *
+	 * @param session
+	 * @param input
+	 * @return
+	 * @throws javax.jms.JMSException
+	 */
+	public Message toMessage(Session session, TridentTuple input) throws JMSException;
+}
---
+++ src/main/java/backtype/storm/contrib/jms/trident/JmsState.java
@@ -0,0 +1,129 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package backtype.storm.contrib.jms.trident;
+
+import backtype.storm.contrib.jms.JmsProvider;
+import backtype.storm.contrib.jms.TridentJmsMessageProducer;
+import backtype.storm.topology.FailedException;
+import backtype.storm.tuple.Values;
+import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import storm.trident.operation.TridentCollector;
+import storm.trident.state.State;
+import storm.trident.tuple.TridentTuple;
+
+import javax.jms.*;
+import java.io.Serializable;
+import java.lang.IllegalStateException;
+import java.util.List;
+import java.util.Map;
+
+public class JmsState implements State {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JmsState.class);
+
+    private Options options;
+    private Connection connection;
+    private Session session;
+    private MessageProducer messageProducer;
+
+    protected JmsState(Options options) {
+        this.options = options;
+    }
+
+    public static class Options implements Serializable {
+        private JmsProvider jmsProvider;
+        private TridentJmsMessageProducer msgProducer;
+        private int jmsAcknowledgeMode = Session.AUTO_ACKNOWLEDGE;
+        private boolean jmsTransactional = true;
+
+        public Options withJmsProvider(JmsProvider provider) {
+            this.jmsProvider = provider;
+            return this;
+        }
+
+        public Options withMessageProducer(TridentJmsMessageProducer msgProducer) {
+            this.msgProducer = msgProducer;
+            return this;
+        }
+
+        public Options withJmsAcknowledgeMode(int jmsAcknowledgeMode) {
+            this.jmsAcknowledgeMode = jmsAcknowledgeMode;
+            return this;
+        }
+
+        public Options withJmsTransactional(boolean jmsTransactional) {
+            this.jmsTransactional = jmsTransactional;
+            return this;
+        }
+    }
+
+    protected void prepare() {
+        if(this.options.jmsProvider == null || this.options.msgProducer == null){
+            throw new IllegalStateException("JMS Provider and MessageProducer not set.");
+        }
+        LOG.debug("Connecting JMS..");
+        try {
+            ConnectionFactory cf = this.options.jmsProvider.connectionFactory();
+            Destination dest = this.options.jmsProvider.destination();
+            this.connection = cf.createConnection();
+            this.session = connection.createSession(this.options.jmsTransactional,
+                    this.options.jmsAcknowledgeMode);
+            this.messageProducer = session.createProducer(dest);
+
+            connection.start();
+        } catch (Exception e) {
+            LOG.warn("Error creating JMS connection.", e);
+        }
+    }
+
+    @Override
+    public void beginCommit(Long aLong) {
+        LOG.debug("beginCommit is noop.");
+    }
+
+    @Override
+    public void commit(Long aLong) {
+        LOG.debug("commit is noop.");
+    }
+
+    public void updateState(List<TridentTuple> tuples, TridentCollector collector) throws JMSException {
+        try {
+        for(TridentTuple tuple : tuples) {
+                Message msg = this.options.msgProducer.toMessage(this.session, tuple);
+                if (msg != null) {
+                    if (msg.getJMSDestination() != null) {
+                        this.messageProducer.send(msg.getJMSDestination(), msg);
+                    } else {
+                        this.messageProducer.send(msg);
+                    }
+                }
+            }
+        } catch (JMSException e) {
+            LOG.warn("Failed to send jmd message for a trident batch ", e);
+            if(this.options.jmsTransactional) {
+                session.rollback();
+            }
+            throw new FailedException("Failed to write tuples", e);
+        }
+        if(this.options.jmsTransactional) {
+            session.commit();
+        }
+    }
+}
