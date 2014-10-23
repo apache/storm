8c449c328d75c72c309d6f1fb3abdaf4d0a8f746
Parth Brahmbhatt <brahmbhatt.parth@gmail.com>
2014-10-23 09:52:14 -0700

    Adding hdfs-conf.yaml to all test cases to support security.

--- external/storm-hdfs/src/test/java/org/apache/storm/hdfs/trident/TridentSequenceTopology.java
+++ external/storm-hdfs/src/test/java/org/apache/storm/hdfs/trident/TridentSequenceTopology.java
@@ -27,13 +27,16 @@ import org.apache.storm.hdfs.common.rotation.MoveFileAction;
 import org.apache.storm.hdfs.trident.format.*;
 import org.apache.storm.hdfs.trident.rotation.FileRotationPolicy;
 import org.apache.storm.hdfs.trident.rotation.FileSizeRotationPolicy;
+
+import org.yaml.snakeyaml.Yaml;
 import org.apache.storm.trident.Stream;
 import org.apache.storm.trident.TridentState;
 import org.apache.storm.trident.TridentTopology;
-import org.apache.storm.trident.operation.BaseFunction;
-import org.apache.storm.trident.operation.TridentCollector;
 import org.apache.storm.trident.state.StateFactory;
-import org.apache.storm.trident.tuple.TridentTuple;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Map;
 
 public class TridentSequenceTopology {
 
@@ -73,14 +76,20 @@ public class TridentSequenceTopology {
     public static void main(String[] args) throws Exception {
         Config conf = new Config();
         conf.setMaxSpoutPending(5);
-        if (args.length == 1) {
+
+        Yaml yaml = new Yaml();
+        InputStream in = new FileInputStream(args[1]);
+        Map<String, Object> yamlConf = (Map<String, Object>) yaml.load(in);
+        in.close();
+        conf.put("hdfs.config", yamlConf);
+
+        if (args.length == 2) {
             LocalCluster cluster = new LocalCluster();
             cluster.submitTopology("wordCounter", conf, buildTopology(args[0]));
             Thread.sleep(120 * 1000);
-        }
-        else if(args.length == 2) {
+        } else if(args.length == 3) {
             conf.setNumWorkers(3);
-            StormSubmitter.submitTopology(args[1], conf, buildTopology(args[0]));
+            StormSubmitter.submitTopology(args[2], conf, buildTopology(args[0]));
         } else{
             System.out.println("Usage: TridentFileTopology <hdfs url> [topology name]");
         }
