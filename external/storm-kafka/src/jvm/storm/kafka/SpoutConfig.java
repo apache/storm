f2d25d3b31a106ccb6f4fb2e6e3ed62dd91d3299
Rick Kellogg <rmkellogg@comcast.net>
2015-02-26 14:37:44 -0500

    Added additional constructor with all required properties. Marked incomplete constructor as deprecated. Extensive Javadocs added.

--- external/storm-kafka/src/jvm/storm/kafka/SpoutConfig.java
+++ external/storm-kafka/src/jvm/storm/kafka/SpoutConfig.java
@@ -19,10 +19,31 @@ package storm.kafka;
 
 import java.util.List;
 
+/**
+ * Configuration passed to constructor of KafkaSpout.
+ * 
+ * As the Spout reads messages from Kafka, it maintains the KafkaSpout offsets within Zookeeper.
+ *   
+ * These offset values are stored under {zkRoot}/{id}.
+ * 
+ * @see KafkaSpout
+ */
 public class SpoutConfig extends KafkaConfig {
+	/**
+	 * List of Zookeeper host names used for Spout offset storage (without port number or other path).
+	 */
     public List<String> zkServers;
+	/**
+	 * Zookeeper port used for Spout offset storage
+	 */
     public Integer zkPort;
+    /**
+     * Path within Zookeeper used for Spout offset data (requires leading slash) 
+     */
     public String zkRoot;
+    /**
+     * Identifier used within Zookeeper to identity unique Spouts.
+     */
     public String id;
 
     // setting for how often to save the current kafka offset to ZooKeeper
@@ -34,8 +55,36 @@ public class SpoutConfig extends KafkaConfig {
     public double retryDelayMultiplier = 1.0;
     public long retryDelayMaxMs = 60 * 1000;
 
+    /**
+     * Construct configuration for KafkaSpout
+     * 
+     * @param hosts Hosts with Kafka topic (required)
+     * @param topic Topic name (required)
+     * @param zkRoot Zookeeper path for storage of Spout offsets (required)
+     * @param id Identifier used to identify Spout (required)
+     * 
+     * @deprecated
+     */
     public SpoutConfig(BrokerHosts hosts, String topic, String zkRoot, String id) {
         super(hosts, topic);
+        this.zkRoot = zkRoot;
+        this.id = id;
+    }
+    
+    /**
+     * Construct configuration for KafkaSpout
+     * 
+     * @param hosts Hosts with Kafka topic (required)
+     * @param topic Topic name (required)
+     * @param zkServers Zookeeper host names for Spout offset storage (required)
+     * @param zkPort Zookeeper port number for Spout offset storage (required)
+     * @param zkRoot Zookeeper path for storage of Spout offsets (required)
+     * @param id Identifier used to identify Spout (required)
+     */
+    public SpoutConfig(BrokerHosts hosts, String topic, List<String> zkServers, int zkPort, String zkRoot, String id) {
+        super(hosts, topic);
+        this.zkServers = zkServers;
+        this.zkPort = zkPort;
         this.zkRoot = zkRoot;
         this.id = id;
     }
