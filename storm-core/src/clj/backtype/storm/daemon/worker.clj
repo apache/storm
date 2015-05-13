6ef2f11e9eade772c8ae67a6410d537871739938
Jungtaek Lim <kabhwan@gmail.com>
2015-05-14 07:50:45 +0900

    While sending tuple, check task->node+port with read lock
    * we can ensure task->node+port is safe within read lock
    ** refer write lock inside of mk-refresh-connections
    * don't group messages from transfer-fn cause we'll match task->node+port later
    * Let TransferDrainer groups message to node+port by referring task id
    ** So then we can still enjoy optimization of sending logic

--- storm-core/src/clj/backtype/storm/daemon/worker.clj
+++ storm-core/src/clj/backtype/storm/daemon/worker.clj
@@ -118,26 +118,17 @@
   (let [local-tasks (-> worker :task-ids set)
         local-transfer (:transfer-local-fn worker)
         ^DisruptorQueue transfer-queue (:transfer-queue worker)
-        task->node+port (:cached-task->node+port worker)
         try-serialize-local ((:storm-conf worker) TOPOLOGY-TESTING-ALWAYS-TRY-SERIALIZE)
         transfer-fn
           (fn [^KryoTupleSerializer serializer tuple-batch]
             (let [local (ArrayList.)
-                  remoteMap (HashMap.)]
+                  remote (ArrayList.)]
               (fast-list-iter [[task tuple :as pair] tuple-batch]
                 (if (local-tasks task)
-                  (.add local pair) 
-
-                  ;;Using java objects directly to avoid performance issues in java code
-                  (let [node+port (get @task->node+port task)]
-                    (when (not (.get remoteMap node+port))
-                      (.put remoteMap node+port (ArrayList.)))
-                    (let [remote (.get remoteMap node+port)]
-                      (.add remote (TaskMessage. task (.serialize serializer tuple)))
-                     )))) 
-                (local-transfer local)
-                (disruptor/publish transfer-queue remoteMap)
-              ))]
+                  (.add local pair)
+                  (.add remote (TaskMessage. task (.serialize serializer tuple)))))
+              (local-transfer local)
+              (disruptor/publish transfer-queue remote)))]
     (if try-serialize-local
       (do 
         (log-warn "WILL TRY TO SERIALIZE ALL TUPLES (Turn off " TOPOLOGY-TESTING-ALWAYS-TRY-SERIALIZE " for production)")
@@ -336,12 +327,13 @@
         ]
     (disruptor/clojure-handler
       (fn [packets _ batch-end?]
-        (.add drainer packets)
-        
+        (.addAll drainer packets)
+
         (when batch-end?
           (read-locked endpoint-socket-lock
-            (let [node+port->socket @node+port->socket]
-              (.send drainer node+port->socket)))
+            (let [node+port->socket @node+port->socket
+                  task->node+port @task->node+port]
+              (.send drainer task->node+port node+port->socket)))
           (.clear drainer))))))
 
 ;; Check whether this messaging connection is ready to send data
--- storm-core/src/jvm/backtype/storm/utils/TransferDrainer.java
+++ storm-core/src/jvm/backtype/storm/utils/TransferDrainer.java
@@ -26,88 +26,47 @@ import backtype.storm.messaging.TaskMessage;
 
 public class TransferDrainer {
 
-  private HashMap<String, ArrayList<ArrayList<TaskMessage>>> bundles = new HashMap();
-  
-  public void add(HashMap<String, ArrayList<TaskMessage>> workerTupleSetMap) {
-    for (String key : workerTupleSetMap.keySet()) {
-      
-      ArrayList<ArrayList<TaskMessage>> bundle = bundles.get(key);
-      if (null == bundle) {
-        bundle = new ArrayList<ArrayList<TaskMessage>>();
-        bundles.put(key, bundle);
-      }
-      
-      ArrayList tupleSet = workerTupleSetMap.get(key);
-      if (null != tupleSet && tupleSet.size() > 0) {
-        bundle.add(tupleSet);
-      }
-    } 
+  private ArrayList<TaskMessage> buffer = new ArrayList<TaskMessage>();
+
+  public void addAll(ArrayList<TaskMessage> tuples) {
+    buffer.addAll(tuples);
   }
   
-  public void send(HashMap<String, IConnection> connections) {
-    for (String hostPort : bundles.keySet()) {
+  public void send(HashMap<Integer, String> taskToHostPort, HashMap<String, IConnection> connections) {
+    HashMap<String, ArrayList<TaskMessage>> messageGroupedByDest = groupMessageByDestination(taskToHostPort);
+    for (String hostPort : messageGroupedByDest.keySet()) {
       IConnection connection = connections.get(hostPort);
       if (null != connection) { 
-        ArrayList<ArrayList<TaskMessage>> bundle = bundles.get(hostPort);
-        Iterator<TaskMessage> iter = getBundleIterator(bundle);
+        ArrayList<TaskMessage> bundle = messageGroupedByDest.get(hostPort);
+        Iterator<TaskMessage> iter = bundle.iterator();
         if (null != iter && iter.hasNext()) {
           connection.send(iter);
         }
       }
     } 
   }
-  
-  private Iterator<TaskMessage> getBundleIterator(final ArrayList<ArrayList<TaskMessage>> bundle) {
-    
-    if (null == bundle) {
-      return null;
+
+  private HashMap<String, ArrayList<TaskMessage>> groupMessageByDestination(HashMap<Integer, String> taskToHostPort) {
+    HashMap<String, ArrayList<TaskMessage>> groupedMessage = new HashMap<String, ArrayList<TaskMessage>>();
+    for (TaskMessage message : buffer) {
+      int taskId = message.task();
+
+      String hostAndPort = taskToHostPort.get(taskId);
+      if (null != hostAndPort) {
+        ArrayList<TaskMessage> messages = groupedMessage.get(hostAndPort);
+
+        if (null == messages) {
+          messages = new ArrayList<TaskMessage>();
+          groupedMessage.put(hostAndPort, messages);
+        }
+
+        messages.add(message);
+      }
     }
-    
-    return new Iterator<TaskMessage> () {
-      
-      private int offset = 0;
-      private int size = 0;
-      {
-        for (ArrayList<TaskMessage> list : bundle) {
-            size += list.size();
-        }
-      }
-      
-      private int bundleOffset = 0;
-      private Iterator<TaskMessage> iter = bundle.get(bundleOffset).iterator();
-      
-      @Override
-      public boolean hasNext() {
-        if (offset < size) {
-          return true;
-        }
-        return false;
-      }
-
-      @Override
-      public TaskMessage next() {
-        TaskMessage msg = null;
-        if (iter.hasNext()) {
-          msg = iter.next(); 
-        } else {
-          bundleOffset++;
-          iter = bundle.get(bundleOffset).iterator();
-          msg = iter.next();
-        }
-        if (null != msg) {
-          offset++;
-        }
-        return msg;
-      }
-
-      @Override
-      public void remove() {
-        throw new RuntimeException("not supported");
-      }
-    };
+    return groupedMessage;
   }
-  
+
   public void clear() {
-    bundles.clear();
+    buffer.clear();
   }
 }
