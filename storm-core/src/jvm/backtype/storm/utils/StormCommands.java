5a4ec36ac17af69297dfc73aaa682b8e43edf5de
Priyank <pshah@hortonworks.com>
2015-07-21 14:23:54 -0700

    Sample code to test for apache commons cli.

--- storm-core/src/jvm/backtype/storm/utils/StormCommands.java
+++ storm-core/src/jvm/backtype/storm/utils/StormCommands.java
@@ -1,5 +1,8 @@
 package backtype.storm.utils;
 
+import org.apache.commons.lang.SystemUtils;
+import org.apache.commons.cli.*;
+
 /**
  * Created by pshah on 7/17/15.
  */
@@ -10,5 +13,18 @@ public class StormCommands {
             System.out.println("Argument ++ is " + arg);
 
         }
+        Options options = new Options();
+        options.addOption("jarcommand", true, "help on jar command");
+        CommandLineParser cmdParser = new DefaultParser();
+        CommandLine cmd = null;
+        try {
+            cmd = cmdParser.parse(options, args);
+        } catch (ParseException pe) {
+            System.out.println("Parse Exception" + pe.getMessage());
+        }
+        System.out.println("Value for jar command is " + cmd.getOptionValue
+                ("jarcommand"));
+        HelpFormatter hf = new HelpFormatter();
+        hf.printHelp("storm", options);
     }
 }
