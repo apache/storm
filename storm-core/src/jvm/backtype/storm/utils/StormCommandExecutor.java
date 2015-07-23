8c2fbeadbf699b5b286479649d133623c92bc85c
Priyank <pshah@hortonworks.com>
2015-07-23 14:49:43 -0700

    Add printUsage mainly.

--- storm-core/src/jvm/backtype/storm/utils/StormCommandExecutor.java
+++ storm-core/src/jvm/backtype/storm/utils/StormCommandExecutor.java
@@ -1,8 +1,11 @@
 package backtype.storm.utils;
 
 import java.io.File;
+import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.SystemUtils;
 
 /**
@@ -23,16 +26,14 @@ abstract class StormCommandExecutor {
     String stormExternalClasspath;
     String stormExternalClasspathDaemon;
     String fileSeparator;
-    final String[] COMMANDS = {"jar", "kill", "shell", "nimbus", "ui",
-            "logviewer",
-            "drpc", "supervisor", "localconfvalue",
-            "remoteconfvalue", "repl", "classpath",
-            "activate", "deactivate", "rebalance", "help",
-            "list", "dev-zookeeper", "version", "monitor",
-            "upload-credentials", "get-errors"};
+    final List<String> COMMANDS = Arrays.asList("jar", "kill", "shell",
+            "nimbus", "ui", "logviewer", "drpc", "supervisor",
+            "localconfvalue",  "remoteconfvalue", "repl", "classpath",
+            "activate", "deactivate", "rebalance", "help",  "list",
+            "dev-zookeeper", "version", "monitor", "upload-credentials",
+            "get-errors");
 
     public static void main(String[] args) {
-        System.out.println("Executing StormCommandExecutor main method");
         for (String arg : args) {
             System.out.println("Argument ++ is " + arg);
         }
@@ -61,7 +62,8 @@ class UnixStormCommandExecutor extends StormCommandExecutor {
     }
 
     void initialize() {
-        this.fileSeparator = System .getProperty ("fileSeparator");
+        Collections.sort(this.COMMANDS);
+        this.fileSeparator = System .getProperty ("file.separator");
         this.stormHomeDirectory = System.getenv("STORM_BASE_DIR");
         this.userConfDirectory = System.getProperty("user.home") +
                 this.fileSeparator + "" +
@@ -106,21 +108,46 @@ class UnixStormCommandExecutor extends StormCommandExecutor {
             System.out.println("******************************************");
             System.exit(1);
         }
-        System.out.println("this.stormHomeDirectory is " + this
-                .stormHomeDirectory);
-        System.out.println("this.userConfDirectory is " + this
-                .userConfDirectory);
-        System.out.println("this.stormConfDirectory is " + this
-                .stormConfDirectory);
         //System.getProperties().list(System.out);
     }
 
     void execute (String[] args) {
         if (args.length == 0) {
-            this.executeHelpCommand();
+            this.printUsage();
             System.exit(-1);
         }
-        return;
+        List<String> commandArgs = new ArrayList<String>();
+        for (int i = 0; i < args.length; ++i) {
+            if (args[i] == "-c") {
+                this.configOptions.add(args[++i]);
+            } else if (args[i] == "--config") {
+                this.configFile = args[++i];
+            } else {
+                commandArgs.add(args[i]);
+            }
+        }
+        if ((commandArgs.size() == 0)  || (!this.COMMANDS.contains
+                (commandArgs.get(0)))) {
+            System.out.println("Unknown command: [storm " + StringUtils.join
+                    (args, " ") +  "]");
+            this.printUsage();
+            System.exit(254);
+
+        }
+
+    }
+
+    private void printUsage() {
+        String commands = StringUtils.join(this.COMMANDS, "\n\t");
+        System.out.println("Commands:\n\t" + commands);
+        System.out.println("\nHelp: \n\thelp \n\thelp <command>\n");
+        System.out.println("Documentation for the storm client can be found" +
+                " at "  +
+                "http://storm.incubator.apache" +
+                ".org/documentation/Command-line-client.html\n");
+        System.out.println("Configs can be overridden using one or more -c " +
+                "flags, e.g. " +
+                "\"storm list -c nimbus.host=nimbus.mycompany.com\"\n");
     }
 
     private void executeHelpCommand() {
