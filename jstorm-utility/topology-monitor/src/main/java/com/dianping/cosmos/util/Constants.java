e8f64d5e88a8f5c29c1633104d06de0970d57676
P. Taylor Goetz <ptgoetz@gmail.com>
2015-11-05 15:23:13 -0500

    remove jstorm-utility directory

--- jstorm-utility/topology-monitor/src/main/java/com/dianping/cosmos/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.dianping.cosmos.util;
-
-public class Constants {
-
-	public static final int EMIT_FREQUENCY_IN_SECONDS = 5;
-	
-	public static final int TPS_COUNTER_FREQUENCY_IN_SECONDS = 30;
-
-}
