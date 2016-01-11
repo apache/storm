97499abdc5fefaafb20748472f97d2766e275583
Dmitry Sergeev <deepnekro@gmail.com>
2016-01-11 17:06:36 +0200

    Fix NPE

--- external/storm-kafka/src/jvm/storm/kafka/KafkaJavaApiSpout.java
+++ external/storm-kafka/src/jvm/storm/kafka/KafkaJavaApiSpout.java
@@ -32,6 +32,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.Lock;
@@ -73,6 +74,7 @@ public class KafkaJavaApiSpout extends BaseRichSpout {
 
     public KafkaJavaApiSpout(SpoutConfig spoutConfig) {
         lock = new ReentrantLock();
+        messages = new ConcurrentHashMap<>();
         this._spoutConfig = spoutConfig;
     }
 
