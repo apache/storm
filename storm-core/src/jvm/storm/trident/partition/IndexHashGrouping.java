8b7b2792eecc847cdb5eca940ff6bc2e38b8ba20
P. Taylor Goetz <ptgoetz@gmail.com>
2016-01-20 15:44:21 -0500

    merge fix for STORM-1481: avoid Math.abs(Integer) get a negative value

--- storm-core/src/jvm/storm/trident/partition/IndexHashGrouping.java
+++ storm-core/src/jvm/storm/trident/partition/IndexHashGrouping.java
@@ -20,15 +20,17 @@ package storm.trident.partition;
 import backtype.storm.generated.GlobalStreamId;
 import backtype.storm.grouping.CustomStreamGrouping;
 import backtype.storm.task.WorkerTopologyContext;
+import backtype.storm.utils.Utils;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class IndexHashGrouping implements CustomStreamGrouping {
     public static int objectToIndex(Object val, int numPartitions) {
-        if(val==null) return 0;
-        else {
-            return Math.abs(val.hashCode()) % numPartitions;
+        if(val == null) {
+            return 0;
         }
+        return Utils.toPositive(val.hashCode()) % numPartitions;
     }
     
     int _index;
