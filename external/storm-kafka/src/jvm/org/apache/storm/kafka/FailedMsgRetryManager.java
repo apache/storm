0f1aec566a6ffa6c29da66ea2969bdb27921baf5
Abhishek Agarwal <abhishek.agarwal@inmobi.com>
2016-04-12 15:28:06 +0530

    STORM-1705: Cap number of retries for a failed message

--- external/storm-kafka/src/jvm/org/apache/storm/kafka/FailedMsgRetryManager.java
+++ external/storm-kafka/src/jvm/org/apache/storm/kafka/FailedMsgRetryManager.java
@@ -17,13 +17,58 @@
  */
 package org.apache.storm.kafka;
 
+import java.io.Serializable;
 import java.util.Set;
 
-public interface FailedMsgRetryManager {
-    public void failed(Long offset);
-    public void acked(Long offset);
-    public void retryStarted(Long offset);
-    public Long nextFailedMessageToRetry();
-    public boolean shouldRetryMsg(Long offset);
-    public Set<Long> clearInvalidMessages(Long kafkaOffset);
+public interface FailedMsgRetryManager extends Serializable {
+
+    /**
+     * Initialization
+     */
+    void prepare(SpoutConfig spoutConfig);
+
+    /**
+     * Message corresponding to the offset failed in kafka spout.
+     * @param offset
+     */
+    void failed(Long offset);
+
+    /**
+     * Message corresponding to the offset, was acked to kafka spout.
+     * @param offset
+     */
+    void acked(Long offset);
+
+    /**
+     * Message corresponding to the offset, has been re-emitted and under transit.
+     * @param offset
+     */
+    void retryStarted(Long offset);
+
+    /**
+     * The offset of message, which is to be re-emitted. Spout will fetch messages starting from this offset
+     * and resend them, except completed messages.
+     * @return
+     */
+    Long nextFailedMessageToRetry();
+
+    /**
+     * @param offset
+     * @return True if the message corresponding to the offset should be emitted NOW. False otherwise.
+     */
+    boolean shouldReEmitMsg(Long offset);
+
+    /**
+     * Spout will clean up the state for this offset if false is returned.
+     * @param offset
+     * @return True if the message will be retried again. False otherwise.
+     */
+    boolean retryFurther(Long offset);
+
+    /**
+     * Clear any offsets before kafkaOffset. These offsets are no longer available in kafka.
+     * @param kafkaOffset
+     * @return Set of offsets removed.
+     */
+    Set<Long> clearOffsetsBefore(Long kafkaOffset);
 }
