4c7117d75f80b820da110c3abee88134ca6ecaaa
Jungtaek Lim <kabhwan@gmail.com>
2016-05-17 20:45:03 +0900

    STORM-1842 Forward references in storm.thrift cause tooling issues
    * python doesn't support forward reference so ttypes.py has a problem regarding this issue
    * reorder HBPulse, HBRecords, HBNodes to resolve forward reference issue

--- storm-core/src/storm.thrift
+++ storm-core/src/storm.thrift
@@ -697,6 +697,19 @@ enum HBServerMessageType {
   NOT_AUTHORIZED
 }
 
+struct HBPulse {
+  1: required string id;
+  2: binary details;
+}
+
+struct HBRecords {
+  1: list<HBPulse> pulses;
+}
+
+struct HBNodes {
+  1: list<string> pulseIds;
+}
+
 union HBMessageData {
   1: string path,
   2: HBPulse pulse,
@@ -712,24 +725,10 @@ struct HBMessage {
   3: optional i32 message_id = -1,
 }
 
-
 exception HBAuthorizationException {
   1: required string msg;
 }
 
 exception HBExecutionException {
   1: required string msg;
-}
-
-struct HBPulse {
-  1: required string id;
-  2: binary details;
-}
-
-struct HBRecords {
-  1: list<HBPulse> pulses;
-}
-
-struct HBNodes {
-  1: list<string> pulseIds;
 }
