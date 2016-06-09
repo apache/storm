5c706afcb0922b891f1eda0f5483c89272491d12
Abhishek Agarwal <abhishek.agarwal@inmobi.com>
2016-06-09 18:36:49 +0530

    STORM-1705: Update storm-kafka readme

--- external/storm-kafka/src/jvm/org/apache/storm/kafka/FailedMsgRetryManager.java
+++ external/storm-kafka/src/jvm/org/apache/storm/kafka/FailedMsgRetryManager.java
@@ -18,6 +18,7 @@
 package org.apache.storm.kafka;
 
 import java.io.Serializable;
+import java.util.Map;
 import java.util.Set;
 
 public interface FailedMsgRetryManager extends Serializable {
@@ -25,7 +26,7 @@ public interface FailedMsgRetryManager extends Serializable {
     /**
      * Initialization
      */
-    void prepare(SpoutConfig spoutConfig);
+    void prepare(SpoutConfig spoutConfig, Map stormConf);
 
     /**
      * Message corresponding to the offset failed in kafka spout.
