bd2ede7e87a7540ce609c9e925091c4e6cf90d06
darionyaphet <darion.yaphet@gmail.com>
2016-06-16 23:29:27 +0800

    STORM-1908 : Support Storm HBase ZooKeeper Config

--- external/storm-hbase/src/main/java/org/apache/storm/hbase/bolt/AbstractHBaseBolt.java
+++ external/storm-hbase/src/main/java/org/apache/storm/hbase/bolt/AbstractHBaseBolt.java
@@ -32,7 +32,6 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
-// TODO support more configuration options, for now we're defaulting to the hbase-*.xml files found on the classpath
 public abstract class AbstractHBaseBolt extends BaseRichBolt {
     private static final Logger LOG = LoggerFactory.getLogger(AbstractHBaseBolt.class);
 
@@ -43,11 +42,31 @@ public abstract class AbstractHBaseBolt extends BaseRichBolt {
     protected HBaseMapper mapper;
     protected String configKey;
 
+    private static final String DEFAULT_ZK_HOSTS = "localhost";
+    private static final String DEFAULT_ZK_PORT = "2181";
+    private static final String DEFAULT_ZK_PARENT = "/hbase";
+
+    private String zkHosts = DEFAULT_ZK_HOSTS;
+    private String zkPort = DEFAULT_ZK_PORT;
+    private String zkParent = DEFAULT_ZK_PARENT;
+
     public AbstractHBaseBolt(String tableName, HBaseMapper mapper) {
         Validate.notEmpty(tableName, "Table name can not be blank or null");
         Validate.notNull(mapper, "mapper can not be null");
         this.tableName = tableName;
         this.mapper = mapper;
+    }
+
+    public void setZkHosts(String hosts) {
+        this.zkHosts = hosts;
+    }
+
+    public void setZkPort(String port) {
+        this.zkPort = port;
+    }
+
+    public void setZkParent(String parent) {
+        this.zkParent = parent;
     }
 
     @Override
@@ -66,6 +85,10 @@ public abstract class AbstractHBaseBolt extends BaseRichBolt {
         for(String key : conf.keySet()) {
             hbConfig.set(key, String.valueOf(conf.get(key)));
         }
+
+        hbConfig.set("hbase.zookeeper.quorum", zkHosts);
+        hbConfig.set("hbase.zookeeper.property.clientPort", zkPort);
+        hbConfig.set("zookeeper.znode.parent", zkParent);
 
         //heck for backward compatibility, we need to pass TOPOLOGY_AUTO_CREDENTIALS to hbase conf
         //the conf instance is instance of persistentMap so making a copy.
