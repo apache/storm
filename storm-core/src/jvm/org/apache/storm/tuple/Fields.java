ee653f54faf16af7bbbbfa6ee448638f46ad30b6
lujinhong <lujinhong2@gmail.com>
2016-06-29 16:56:55 +0800

    use fieldIndex()

--- storm-core/src/jvm/org/apache/storm/tuple/Fields.java
+++ storm-core/src/jvm/org/apache/storm/tuple/Fields.java
@@ -51,7 +51,7 @@ public class Fields implements Iterable<String>, Serializable {
     public List<Object> select(Fields selector, List<Object> tuple) {
         List<Object> ret = new ArrayList<>(selector.size());
         for (String s : selector) {
-       	 	ret.add(tuple.get(fieldIndex(s))); 
+            ret.add(tuple.get(fieldIndex(s))); 
         }
         return ret;
     }
