166f780f1ced00ced56c1749b8304b9174e40ab9
Jungtaek Lim <kabhwan@gmail.com>
2016-07-13 19:23:33 +0900

    STORM-1966 Expand metric having Map type as value into multiple metrics based on entries
    * populate metric before applying filter so that filter can play with populated metrics
    * add relevant configs to metrics consumer registration and describe them to storm.config.yaml
    * add unit test

--- storm-core/src/jvm/org/apache/storm/metric/api/IMetricsConsumer.java
+++ storm-core/src/jvm/org/apache/storm/metric/api/IMetricsConsumer.java
@@ -21,6 +21,7 @@ import org.apache.storm.task.IErrorReporter;
 import org.apache.storm.task.TopologyContext;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 
 public interface IMetricsConsumer {
     public static class TaskInfo {
@@ -54,6 +55,23 @@ public interface IMetricsConsumer {
         }
         public String name; 
         public Object value;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof DataPoint)) return false;
+
+            DataPoint dataPoint = (DataPoint) o;
+
+            return Objects.equals(name, dataPoint.name) && Objects.deepEquals(value, dataPoint.value);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name != null ? name.hashCode() : 0;
+            result = 31 * result + (value != null ? value.hashCode() : 0);
+            return result;
+        }
     }
 
     void prepare(Map stormConf, Object registrationArgument, TopologyContext context, IErrorReporter errorReporter);
