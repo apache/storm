7c485cbd609f23ea1665a4f25f7302162c7a46ac
Hugo Louro <hmclouro@gmail.com>
2016-07-13 09:06:10 -0700

    BUG-62185: Build Fails Upon Merging Kafka Topics and Kafka Trident Spout Patches

--- external/storm-kafka-client/src/main/java/org/apache/storm/kafka/spout/KafkaSpoutStreamsWildcardTopics.java
+++ external/storm-kafka-client/src/main/java/org/apache/storm/kafka/spout/KafkaSpoutStreamsWildcardTopics.java
@@ -41,6 +41,11 @@ public class KafkaSpoutStreamsWildcardTopics implements KafkaSpoutStreams {
     }
 
     @Override
+    public Fields getOutputFields() {
+        return kafkaSpoutStream.getOutputFields();
+    }
+
+    @Override
     public void emit(SpoutOutputCollector collector, List<Object> tuple, KafkaSpoutMessageId messageId) {
         kafkaSpoutStream.emit(collector, tuple, messageId);
     }
