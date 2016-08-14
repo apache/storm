df17f5445ca89a9001589a829c89eccae9d076fd
Jungtaek Lim <kabhwan@gmail.com>
2016-08-14 23:20:47 +0900

    Merge branch '1.x-STORM-1913' of https://github.com/knusbaum/incubator-storm into STORM-1913-1.x

--- storm-core/src/jvm/org/apache/storm/topology/BaseConfigurationDeclarer.java
+++ storm-core/src/jvm/org/apache/storm/topology/BaseConfigurationDeclarer.java
@@ -46,7 +46,7 @@ public abstract class BaseConfigurationDeclarer<T extends ComponentConfiguration
         if(val!=null) val = val.intValue();
         return addConfiguration(Config.TOPOLOGY_MAX_SPOUT_PENDING, val);
     }
-    
+
     @Override
     public T setNumTasks(Number val) {
         if (val != null) val = val.intValue();
@@ -55,16 +55,18 @@ public abstract class BaseConfigurationDeclarer<T extends ComponentConfiguration
 
     @Override
     public T setMemoryLoad(Number onHeap) {
-        return setMemoryLoad(onHeap, Utils.getDouble(conf.get(Config.TOPOLOGY_COMPONENT_RESOURCES_OFFHEAP_MEMORY_MB)));
+        if (onHeap != null) {
+            onHeap = onHeap.doubleValue();
+            return addConfiguration(Config.TOPOLOGY_COMPONENT_RESOURCES_ONHEAP_MEMORY_MB, onHeap);
+        }
+        return null;
     }
 
     @Override
     public T setMemoryLoad(Number onHeap, Number offHeap) {
         T ret = null;
-        if (onHeap != null) {
-            onHeap = onHeap.doubleValue();
-            ret = addConfiguration(Config.TOPOLOGY_COMPONENT_RESOURCES_ONHEAP_MEMORY_MB, onHeap);
-        }
+        ret = setMemoryLoad(onHeap);
+
         if (offHeap!=null) {
             offHeap = offHeap.doubleValue();
             ret = addConfiguration(Config.TOPOLOGY_COMPONENT_RESOURCES_OFFHEAP_MEMORY_MB, offHeap);
