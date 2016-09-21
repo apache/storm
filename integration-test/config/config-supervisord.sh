1b231f5ef9d57b41ab5bb312e69df9eed4b4231c
Raghav Kumar Gautam <raghav@apache.org>
2016-09-21 12:09:46 -0700

    addressing rat issues

--- integration-test/config/config-supervisord.sh
+++
@@ -1,5 +0,0 @@
-echo [program:storm-$1] | sudo tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo command=storm $1 | sudo tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo directory=/home/storm | sudo tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo autorestart=true | sudo tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo user=storm | sudo tee -a /etc/supervisor/conf.d/storm-$1.conf
--- integration-test/config/user-script.sh
+++
@@ -1,1 +0,0 @@
-apt-get --yes install emacs tmux
