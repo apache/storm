905c443653f579cd0af96d1c799843a1b718801f
Robert (Bobby) Evans <evans@yahoo-inc.com>
2016-09-26 09:21:32 -0500

    Bad include for JDK 7 support

--- storm-core/src/jvm/org/apache/storm/daemon/supervisor/BasicContainer.java
+++ storm-core/src/jvm/org/apache/storm/daemon/supervisor/BasicContainer.java
@@ -30,7 +30,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.storm.Config;
