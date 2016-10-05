4a2f50cfe2dc770eb1ff9b31276c6eceac12f8cd
P. Taylor Goetz <ptgoetz@gmail.com>
2016-10-05 14:05:08 -0400

    storm-jms: split into multi-module project

---
+++ external/storm-jms/core/src/main/java/org/apache/storm/jms/trident/JmsBatch.java
@@ -0,0 +1,10 @@
+package org.apache.storm.jms.trident;
+
+/**
+ * Batch coordination metadata object for the TridentJmsSpout.
+ * This implementation does not use batch metadata, so the object is empty.
+ *
+ */
+public class JmsBatch {
+    // Empty class
+}
