81cd0745260ca066d9cf5e73ac4a34fa9ef60d55
Kyle Nusbaum <KyleJNusbaum@gmail.com>
2016-10-11 11:01:31 -0500

    Merge branch 'STORM-2131' of https://github.com/knusbaum/incubator-storm

--- storm-core/src/jvm/org/apache/storm/daemon/supervisor/SupervisorUtils.java
+++ storm-core/src/jvm/org/apache/storm/daemon/supervisor/SupervisorUtils.java
@@ -96,9 +96,19 @@ public class SupervisorUtils {
 
     public static void setupStormCodeDir(Map<String, Object> conf, Map<String, Object> stormConf, String dir) throws IOException {
         if (Utils.getBoolean(conf.get(Config.SUPERVISOR_RUN_WORKER_AS_USER), false)) {
-            String logPrefix = "setup conf for " + dir;
+            String logPrefix = "Storm Code Dir Setup for " + dir;
             List<String> commands = new ArrayList<>();
             commands.add("code-dir");
+            commands.add(dir);
+            processLauncherAndWait(conf, (String) (stormConf.get(Config.TOPOLOGY_SUBMITTER_USER)), commands, null, logPrefix);
+        }
+    }
+
+    public static void setupWorkerArtifactsDir(Map<String, Object> conf, Map<String, Object> stormConf, String dir) throws IOException {
+        if (Utils.getBoolean(conf.get(Config.SUPERVISOR_RUN_WORKER_AS_USER), false)) {
+            String logPrefix = "Worker Artifacts Setup for " + dir;
+            List<String> commands = new ArrayList<>();
+            commands.add("artifacts-dir");
             commands.add(dir);
             processLauncherAndWait(conf, (String) (stormConf.get(Config.TOPOLOGY_SUBMITTER_USER)), commands, null, logPrefix);
         }
