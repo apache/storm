7de6eedb455e3916aebaf561b180dbc9247ee4b6
P. Taylor Goetz <ptgoetz@gmail.com>
2016-10-25 14:45:43 -0400

    STORM-2174: Apply ALv2 license headers

--- external/storm-beam/src/main/java/org/apache/storm/beam/translation/runtime/WindowBolt.java
+++ external/storm-beam/src/main/java/org/apache/storm/beam/translation/runtime/WindowBolt.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.storm.beam.translation.runtime;
 
 import com.google.common.collect.Lists;
@@ -15,9 +32,6 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Map;
 
-/**
- *
- */
 public class WindowBolt extends BaseWindowedBolt {
     private static final Logger LOG = LoggerFactory.getLogger(WindowBolt.class);
 
@@ -32,7 +46,6 @@ public class WindowBolt extends BaseWindowedBolt {
     @Override
     public void execute(TupleWindow inputWindow) {
         LOG.info("*******************Executing window with size {}", inputWindow.get().size());
-        LOG.info("Type: {}", inputWindow.get().getClass());
         List values = Lists.newArrayList();
         for(Tuple t :inputWindow.get()){
             values.add(t.getValue(0));
