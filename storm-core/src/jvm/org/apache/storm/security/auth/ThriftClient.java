6c9d072375496bb2542db9ba664cb19b204dad5a
Satish Duggana <sduggana@hortonworks.com>
2016-11-10 09:35:31 +0530

    STORM-2197: Fix for NimbusClient connectins leak due to ThriftClient connection's leakage in case of errors.

--- storm-core/src/jvm/org/apache/storm/security/auth/ThriftClient.java
+++ storm-core/src/jvm/org/apache/storm/security/auth/ThriftClient.java
@@ -74,9 +74,10 @@ public class ThriftClient implements AutoCloseable {
     }
     
     public synchronized void reconnect() {
-        close();    
+        close();
+        TSocket socket = null;
         try {
-            TSocket socket = new TSocket(_host, _port);
+            socket = new TSocket(_host, _port);
             if(_timeout!=null) {
                 socket.setTimeout(_timeout);
             }
@@ -97,7 +98,13 @@ public class ThriftClient implements AutoCloseable {
                                       Utils.getInt(_conf.get(Config.STORM_NIMBUS_RETRY_INTERVAL_CEILING)),
                                       _retryForever);
             _transport = connectionRetry.doConnectWithRetry(transportPlugin, socket, _host, _asUser);
-        } catch (IOException ex) {
+        } catch (Exception ex) {
+            // close the socket, which releases connection if it has created any.
+            if(socket != null) {
+                try {
+                    socket.close();
+                } catch (Exception e) {}
+            }
             throw new RuntimeException(ex);
         }
         _protocol = null;
