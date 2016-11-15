d1ffee2a29194afdd74ab6de4b401c72fe4760ee
Jungtaek Lim <kabhwan@gmail.com>
2016-11-15 12:55:44 +0900

    STORM-2200 [Storm SQL] Drop Aggregate & Join support on Trident mode
    * Dropping aggregate & join support based on discussion
      * also addresses documentation
    * These implementation will be reused when we introduce them again

--- external/sql/storm-sql-core/src/jvm/org/apache/storm/sql/compiler/backends/trident/TridentLogicalPlanCompiler.java
+++ external/sql/storm-sql-core/src/jvm/org/apache/storm/sql/compiler/backends/trident/TridentLogicalPlanCompiler.java
@@ -20,80 +20,33 @@ package org.apache.storm.sql.compiler.backends.trident;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
-import com.google.common.primitives.Primitives;
 import org.apache.calcite.DataContext;
 import org.apache.calcite.adapter.java.JavaTypeFactory;
 import org.apache.calcite.linq4j.tree.BlockStatement;
-import org.apache.calcite.plan.RelOptUtil;
 import org.apache.calcite.rel.RelNode;
-import org.apache.calcite.rel.core.Aggregate;
-import org.apache.calcite.rel.core.AggregateCall;
 import org.apache.calcite.rel.core.Filter;
-import org.apache.calcite.rel.core.Join;
 import org.apache.calcite.rel.core.Project;
 import org.apache.calcite.rel.core.TableModify;
 import org.apache.calcite.rel.core.TableScan;
-import org.apache.calcite.rel.logical.LogicalJoin;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rex.RexBuilder;
 import org.apache.calcite.rex.RexNode;
-import org.apache.calcite.schema.impl.AggregateFunctionImpl;
-import org.apache.calcite.sql.SqlAggFunction;
-import org.apache.calcite.sql.validate.SqlUserDefinedAggFunction;
-import org.apache.calcite.util.Pair;
 import org.apache.storm.sql.compiler.PostOrderRelNodeVisitor;
 import org.apache.storm.sql.compiler.RexNodeToBlockStatementCompiler;
 import org.apache.storm.sql.runtime.ISqlTridentDataSource;
 import org.apache.storm.sql.runtime.trident.functions.EvaluationFilter;
 import org.apache.storm.sql.runtime.trident.functions.EvaluationFunction;
 import org.apache.storm.sql.runtime.trident.functions.ForwardFunction;
-import org.apache.storm.sql.runtime.trident.functions.UDAFWrappedAggregator;
-import org.apache.storm.sql.runtime.trident.operations.CountBy;
-import org.apache.storm.sql.runtime.trident.operations.DivideForAverage;
-import org.apache.storm.sql.runtime.trident.operations.MaxBy;
-import org.apache.storm.sql.runtime.trident.operations.MinBy;
-import org.apache.storm.sql.runtime.trident.operations.SumBy;
-import org.apache.storm.trident.JoinOutFieldsMode;
-import org.apache.storm.trident.JoinType;
 import org.apache.storm.trident.Stream;
 import org.apache.storm.trident.TridentTopology;
-import org.apache.storm.trident.fluent.ChainedAggregatorDeclarer;
-import org.apache.storm.trident.fluent.GroupedStream;
 import org.apache.storm.trident.fluent.IAggregatableStream;
-import org.apache.storm.trident.operation.Function;
-import org.apache.storm.trident.operation.builtin.Count;
 import org.apache.storm.tuple.Fields;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
-class TransformInformation {
-    private Fields inputFields;
-    private Function function;
-    private Fields functionFields;
-
-    public TransformInformation(Fields inputFields, Function function, Fields functionFields) {
-        this.inputFields = inputFields;
-        this.function = function;
-        this.functionFields = functionFields;
-    }
-
-    public Fields getInputFields() {
-        return inputFields;
-    }
-
-    public Function getFunction() {
-        return function;
-    }
-
-    public Fields getFunctionFields() {
-        return functionFields;
-    }
-}
 
 public class TridentLogicalPlanCompiler extends PostOrderRelNodeVisitor<IAggregatableStream> {
     protected final Map<String, ISqlTridentDataSource> sources;
@@ -149,56 +102,6 @@ public class TridentLogicalPlanCompiler extends PostOrderRelNodeVisitor<IAggrega
                 .partitionPersist(consumer.getStateFactory(), new Fields(inputFields), consumer.getStateUpdater(),
                         new Fields(outputFields))
                 .newValuesStream().name(stageName);
-    }
-
-    @Override
-    public IAggregatableStream visitJoin(Join join, List<IAggregatableStream> inputStreams) throws Exception {
-        if (inputStreams.size() != 2) {
-            throw new RuntimeException("Join is a BiRel");
-        }
-
-        int[] ordinals = new int[2];
-        if (!RelOptUtil.analyzeSimpleEquiJoin((LogicalJoin) join, ordinals)) {
-            throw new UnsupportedOperationException("Only simple equi joins are supported");
-        }
-
-        List<JoinType> joinTypes = new ArrayList<>();
-        switch (join.getJoinType()) {
-            case INNER:
-                joinTypes.add(JoinType.INNER);
-                joinTypes.add(JoinType.INNER);
-                break;
-
-            case LEFT:
-                joinTypes.add(JoinType.INNER);
-                joinTypes.add(JoinType.OUTER);
-                break;
-
-            case RIGHT:
-                joinTypes.add(JoinType.OUTER);
-                joinTypes.add(JoinType.INNER);
-                break;
-
-            case FULL:
-                joinTypes.add(JoinType.OUTER);
-                joinTypes.add(JoinType.OUTER);
-                break;
-
-            default:
-                throw new UnsupportedOperationException("Unsupported join type: " + join.getJoinType());
-        }
-
-        String leftJoinFieldName = join.getLeft().getRowType().getFieldNames().get(ordinals[0]);
-        String rightJoinFieldName = join.getRight().getRowType().getFieldNames().get(ordinals[1]);
-
-        Stream leftInputStream = inputStreams.get(0).toStream();
-        Stream rightInputStream = inputStreams.get(1).toStream();
-        String stageName = getStageName(join);
-
-        return topology
-                .join(leftInputStream, new Fields(leftJoinFieldName), rightInputStream, new Fields(rightJoinFieldName),
-                        new Fields(join.getRowType().getFieldNames()), joinTypes, JoinOutFieldsMode.PRESERVE)
-                .name(stageName);
     }
 
     @Override
@@ -262,215 +165,7 @@ public class TridentLogicalPlanCompiler extends PostOrderRelNodeVisitor<IAggrega
         }
     }
 
-    @Override
-    public IAggregatableStream visitAggregate(Aggregate aggregate, List<IAggregatableStream> inputStreams) throws Exception {
-        if (inputStreams.size() != 1) {
-            throw new RuntimeException("Aggregate is a SingleRel");
-        }
-
-        Stream inputStream = inputStreams.get(0).toStream();
-        String stageName = getStageName(aggregate);
-
-        List<String> outputFieldNames = aggregate.getRowType().getFieldNames();
-
-        List<String> groupByFieldNames = new ArrayList<>();
-        for (Integer idx : aggregate.getGroupSet()) {
-            String fieldName = inputStream.getOutputFields().get(idx);
-            groupByFieldNames.add(fieldName);
-        }
-
-        Fields groupByFields = new Fields(groupByFieldNames);
-        GroupedStream groupedStream = inputStream.groupBy(groupByFields);
-
-        ChainedAggregatorDeclarer chainedAggregatorDeclarer = groupedStream.chainedAgg();
-        List<TransformInformation> transformsAfterChained = new ArrayList<>();
-
-        // Trident doesn't allow duplicated field name... need to do the trick...
-        List<String> temporaryOutputFieldNames = new ArrayList<>();
-        List<String> originOutputFieldNames = new ArrayList<>();
-        for (Pair<AggregateCall, String> aggCallToOutFieldName : aggregate.getNamedAggCalls()) {
-            AggregateCall call = aggCallToOutFieldName.getKey();
-            String outFileName = aggCallToOutFieldName.getValue();
-            String tempOutFileName = "__" + outFileName + "__";
-            temporaryOutputFieldNames.add(tempOutFileName);
-            originOutputFieldNames.add(outFileName);
-            appendAggregationInChain(chainedAggregatorDeclarer, groupByFieldNames, tempOutFileName, inputStream, call, transformsAfterChained);
-        }
-
-        Stream stream = chainedAggregatorDeclarer.chainEnd();
-        for (TransformInformation transformInformation : transformsAfterChained) {
-            stream = stream.each(transformInformation.getInputFields(), transformInformation.getFunction(), transformInformation.getFunctionFields());
-        }
-
-        return stream
-                .each(new Fields(temporaryOutputFieldNames), new ForwardFunction(), new Fields(originOutputFieldNames))
-                .project(new Fields(outputFieldNames)).name(stageName);
-    }
-
-    private void appendAggregationInChain(ChainedAggregatorDeclarer chainedDeclarer, List<String> groupByFieldNames,
-                                          String outputFieldName, Stream inputStream, AggregateCall call, List<TransformInformation> transformsAfterChained) {
-        SqlAggFunction aggFunction = call.getAggregation();
-        String aggregationName = call.getAggregation().getName();
-
-        // only count can have no argument
-        if (call.getArgList().size() != 1) {
-            if (aggregationName.toUpperCase().equals("COUNT")) {
-                if (call.getArgList().size() > 0) {
-                    throw new IllegalArgumentException("COUNT should have one or no(a.k.a '*') argument");
-                }
-            } else {
-                throw new IllegalArgumentException("Aggregate call should have one argument");
-            }
-        }
-
-        if (aggFunction instanceof SqlUserDefinedAggFunction) {
-            appendUDAFToChain(chainedDeclarer, groupByFieldNames, inputStream, call, outputFieldName);
-        } else {
-            switch (aggregationName.toUpperCase()) {
-                case "COUNT":
-                    appendCountFunctionToChain(chainedDeclarer, groupByFieldNames, inputStream, call, outputFieldName);
-                    break;
-
-                case "MAX":
-                    appendMaxFunctionToChain(chainedDeclarer, groupByFieldNames, inputStream, call, outputFieldName);
-                    break;
-
-                case "MIN":
-                    appendMinFunctionToChain(chainedDeclarer, groupByFieldNames, inputStream, call, outputFieldName);
-                    break;
-
-                case "SUM":
-                    appendSumFunctionToChain(chainedDeclarer, groupByFieldNames, inputStream, call, outputFieldName);
-                    break;
-
-                case "AVG":
-                    appendAvgFunctionToChain(chainedDeclarer, groupByFieldNames, inputStream, call, outputFieldName, transformsAfterChained);
-                    break;
-
-                default:
-                    throw new UnsupportedOperationException("Not supported function: " + aggregationName.toUpperCase());
-            }
-        }
-    }
-
-    private void appendUDAFToChain(ChainedAggregatorDeclarer chainedDeclarer, List<String> groupByFieldNames, Stream inputStream, 
-                                   AggregateCall call, String outputField) {
-        AggregateFunctionImpl aggFunction = (AggregateFunctionImpl) ((SqlUserDefinedAggFunction) call.getAggregation()).function;
-
-        List<String> inputFields = new ArrayList<>(groupByFieldNames);
-        Integer fieldIdx = call.getArgList().get(0);
-        String inputFieldName = inputStream.getOutputFields().get(fieldIdx);
-        inputFields.add(inputFieldName);
-
-        Class<?> capturedTargetClazz = getAggrCallReturnJavaType(call);
-
-        List<String> outputFields = new ArrayList<>(groupByFieldNames);
-        outputFields.add(outputField);
-
-        UDAFWrappedAggregator aggregator = new UDAFWrappedAggregator(aggFunction.declaringClass.getName(),
-                aggFunction.isStatic, inputFieldName, capturedTargetClazz);
-
-        chainedDeclarer.aggregate(new Fields(inputFields), aggregator, new Fields(outputField));
-    }
-
-    private void appendAvgFunctionToChain(ChainedAggregatorDeclarer chainedDeclarer, List<String> groupByFieldNames,
-                                          Stream inputStream, AggregateCall call, String outputField,
-                                          List<TransformInformation> transformsAfterChained) {
-        List<String> inputFields = new ArrayList<>(groupByFieldNames);
-        Integer fieldIdx = call.getArgList().get(0);
-        String inputFieldName = inputStream.getOutputFields().get(fieldIdx);
-        inputFields.add(inputFieldName);
-
-        Class<?> capturedTargetClazz = getAggrCallReturnJavaType(call);
-        if (!Number.class.isAssignableFrom(capturedTargetClazz)) {
-            throw new IllegalStateException("Return type of aggregation call should be a Number");
-        }
-
-        Class<? extends Number> targetClazz = (Class<? extends Number>) capturedTargetClazz;
-
-        String tempCountFieldName = "__cnt__" + outputField;
-        String tempSumFieldName = "__sum__" + outputField;
-
-        chainedDeclarer
-                .aggregate(new Fields(inputFields), new CountBy(inputFieldName), new Fields(tempCountFieldName))
-                .aggregate(new Fields(inputFields), new SumBy(inputFieldName, targetClazz), new Fields(tempSumFieldName));
-
-        TransformInformation divForAverage = new TransformInformation(
-                new Fields(tempSumFieldName, tempCountFieldName),
-                new DivideForAverage(targetClazz),
-                new Fields(outputField));
-        transformsAfterChained.add(divForAverage);
-    }
-
-    private void appendCountFunctionToChain(ChainedAggregatorDeclarer chainedDeclarer, List<String> groupByFieldNames,
-                                            Stream inputStream, AggregateCall call, String outputField) {
-        List<String> inputFields = new ArrayList<>(groupByFieldNames);
-        if (call.getArgList().size() == 0) {
-            chainedDeclarer.aggregate(new Fields(inputFields), new Count(), new Fields(outputField));
-        } else {
-            // call.getArgList().size() == 1
-            Integer fieldIdx = call.getArgList().get(0);
-            String inputFieldName = inputStream.getOutputFields().get(fieldIdx);
-            inputFields.add(inputFieldName);
-            chainedDeclarer.aggregate(new Fields(inputFields), new CountBy(inputFieldName), new Fields(outputField));
-        }
-    }
-
-    private void appendMaxFunctionToChain(ChainedAggregatorDeclarer chainedDeclarer, List<String> groupByFieldNames,
-                                          Stream inputStream, AggregateCall call, String outputField) {
-        List<String> inputFields = new ArrayList<>(groupByFieldNames);
-        Integer fieldIdx = call.getArgList().get(0);
-        String inputFieldName = inputStream.getOutputFields().get(fieldIdx);
-        inputFields.add(inputFieldName);
-
-        Class<?> capturedTargetClazz = getAggrCallReturnJavaType(call);
-
-        chainedDeclarer.aggregate(new Fields(inputFields), new MaxBy(inputFieldName, capturedTargetClazz), new Fields(outputField));
-    }
-
-    private void appendMinFunctionToChain(ChainedAggregatorDeclarer chainedDeclarer, List<String> groupByFieldNames, Stream inputStream, AggregateCall call, String outputField) {
-        List<String> inputFields = new ArrayList<>(groupByFieldNames);
-        Integer fieldIdx = call.getArgList().get(0);
-        String inputFieldName = inputStream.getOutputFields().get(fieldIdx);
-        inputFields.add(inputFieldName);
-
-        Class<?> capturedTargetClazz = getAggrCallReturnJavaType(call);
-
-        chainedDeclarer.aggregate(new Fields(inputFields), new MinBy(inputFieldName, capturedTargetClazz), new Fields(outputField));
-    }
-
-    private void appendSumFunctionToChain(ChainedAggregatorDeclarer chainedDeclarer, List<String> groupByFieldNames, Stream inputStream, AggregateCall call, String outputField) {
-        List<String> inputFields = new ArrayList<>(groupByFieldNames);
-        Integer fieldIdx = call.getArgList().get(0);
-        String inputFieldName = inputStream.getOutputFields().get(fieldIdx);
-        inputFields.add(inputFieldName);
-
-        Class<?> capturedTargetClazz = getAggrCallReturnJavaType(call);
-        if (!Number.class.isAssignableFrom(capturedTargetClazz)) {
-            throw new IllegalStateException("Return type of aggregation call should be a Number");
-        }
-
-        Class<? extends Number> targetClazz = (Class<? extends Number>) capturedTargetClazz;
-
-        List<String> outputFields = new ArrayList<>(groupByFieldNames);
-        outputFields.add(outputField);
-        chainedDeclarer.aggregate(new Fields(inputFields), new SumBy(inputFieldName, targetClazz), new Fields(outputField));
-    }
-
     private String getStageName(RelNode n) {
         return n.getClass().getSimpleName().toUpperCase() + "_" + n.getId();
-    }
-
-    private Class<?> getAggrCallReturnJavaType(AggregateCall call) {
-        return toClassWithBoxing(typeFactory.getJavaClass(call.getType()));
-    }
-
-    private Class toClassWithBoxing(Type type) {
-        Class clazz = (Class<?>)type;
-        if (clazz.isPrimitive()) {
-            clazz = Primitives.wrap(clazz);
-        }
-
-        return clazz;
     }
 }
