5539462c16608456b3551e0432c07f5966d96a80
Hugo Louro <hmclouro@gmail.com>
2016-12-02 16:11:33 -0800

    STORM-2223: PMMLBolt   - PMML Bolt supporting pluggable runners and configurable stream output fields   - JPMML runner implementation   - JPMML Prediction Test Topology   - README   - LICENSE information

--- external/storm-kafka-client/src/test/java/org/apache/storm/kafka/spout/test/KafkaSpoutTopologyMainNamedTopics.java
+++ external/storm-kafka-client/src/test/java/org/apache/storm/kafka/spout/test/KafkaSpoutTopologyMainNamedTopics.java
@@ -39,7 +39,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import static org.apache.storm.kafka.spout.KafkaSpoutConfig.FirstPollOffsetStrategy.EARLIEST;
 
