a5fb685fb18b7ad2bd6c454ed9634bd91126098c
Jungtaek Lim <kabhwan@gmail.com>
2017-02-27 21:01:13 +0900

    STORM-2383 [storm-hbase] Support HBase as state backend
    * Implement HBase state backend
      * picked Redis state backend implementation
      * also implemented state iterator
    * Added 'how to set up' to State-checkpointing doc
    * address @arunmahadevan's review comment

--- external/storm-hbase/src/main/java/org/apache/storm/hbase/common/HBaseClient.java
+++ external/storm-hbase/src/main/java/org/apache/storm/hbase/common/HBaseClient.java
@@ -19,6 +19,7 @@ package org.apache.storm.hbase.common;
 
 import com.google.common.collect.Lists;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.security.UserProvider;
 import org.apache.storm.hbase.bolt.mapper.HBaseProjectionCriteria;
@@ -83,6 +84,19 @@ public class HBaseClient implements Closeable{
             mutations.add(inc);
         }
 
+        if (cols.hasColumnsToDelete()) {
+            Delete delete = new Delete(rowKey);
+            delete.setDurability(durability);
+            for (ColumnList.Column col : cols.getColumnsToDelete()) {
+                if (col.getTs() > 0) {
+                    delete.addColumn(col.getFamily(), col.getQualifier(), col.getTs());
+                } else {
+                    delete.addColumn(col.getFamily(), col.getQualifier());
+                }
+            }
+            mutations.add(delete);
+        }
+
         if (mutations.isEmpty()) {
             mutations.add(new Put(rowKey));
         }
@@ -124,6 +138,32 @@ public class HBaseClient implements Closeable{
             return table.get(gets);
         } catch (Exception e) {
             LOG.warn("Could not perform HBASE lookup.", e);
+            throw e;
+        }
+    }
+
+    public ResultScanner scan(byte[] startRow, byte[] stopRow) throws Exception {
+        try {
+            if (startRow == null) {
+                startRow = HConstants.EMPTY_START_ROW;
+            }
+            if (stopRow == null) {
+                stopRow = HConstants.EMPTY_END_ROW;
+            }
+
+            Scan scan = new Scan(startRow, stopRow);
+            return table.getScanner(scan);
+        } catch (Exception e) {
+            LOG.warn("Could not open HBASE scanner.", e);
+            throw e;
+        }
+    }
+
+    public boolean exists(Get get) throws Exception {
+        try {
+            return table.exists(get);
+        } catch (Exception e) {
+            LOG.warn("Could not perform HBASE existence check.", e);
             throw e;
         }
     }
