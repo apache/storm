035c6d17ecb1f1080e364243a76a87937eeaa9f1
ikashperskyi <zeiss131@gmail.com>
2017-03-04 15:10:27 +0100

    STORM-2396: setting interrupted status back up before rethrowing a RuntimeException.

--- storm-core/src/jvm/org/apache/storm/utils/Utils.java
+++ storm-core/src/jvm/org/apache/storm/utils/Utils.java
@@ -335,6 +335,7 @@ public class Utils {
         try {
             Time.sleep(millis);
         } catch(InterruptedException e) {
+            Thread.currentThread().interrupt();
             throw new RuntimeException(e);
         }
     }
