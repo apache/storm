29c761908ec8305d36535c1d2e3d1c0c3d6c95d1
Erik Weathers <erikdw@gmail.com>
2017-05-12 02:27:10 -0700

    [STORM-2510] update checkstyle configuration to lower violations
    1. Update from checkstyle-6.11.2 to checkstyle-7.7.
    2. Tweak google_checks.xml from checkstyle-7.7 to have the following changes that
    are more inline with the way the storm code is written:
      * 4 space indents instead of 2
      * line-length limit is 120 instead of 100
    3. Exclude the generated thrift code in storm-client from being checked.
    4. Update all maxAllowedViolations to be in-sync with the current number of
    violations, through use of the `update-all-pom-violations.bash` script that
    is attached to STORM-2510.
    
    NOTE: this also fixes STORM-2507.

--- storm-server/src/main/java/org/apache/storm/logging/filters/AccessLoggingFilter.java
+++ storm-server/src/main/java/org/apache/storm/logging/filters/AccessLoggingFilter.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.storm.logging.filters;
 import java.io.IOException;
 import javax.servlet.Filter;
