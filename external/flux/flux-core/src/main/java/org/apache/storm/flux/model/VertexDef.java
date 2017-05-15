1891210f39d2de5a8ab4be37ed6f78cf7b720359
Angus Helm <angus.helm@slalom.com>
2017-05-15 11:13:31 -0500

    STORM-2484 Add support for configuring memory load and cpu load in Flux

--- external/flux/flux-core/src/main/java/org/apache/storm/flux/model/VertexDef.java
+++ external/flux/flux-core/src/main/java/org/apache/storm/flux/model/VertexDef.java
@@ -23,8 +23,12 @@ package org.apache.storm.flux.model;
  */
 public abstract class VertexDef extends BeanDef {
 
-    // default parallelism to 1 so if it's ommitted, the topology will still function.
+    // default parallelism to 1 so if it's omitted, the topology will still function.
     private int parallelism = 1;
+    private int numTasks = -1;
+    private int onHeapMemoryLoad = -1;
+    private int offHeapMemoryLoad = -1;
+    private int cpuLoad = -1;
 
     public int getParallelism() {
         return parallelism;
@@ -32,5 +36,37 @@ public abstract class VertexDef extends BeanDef {
 
     public void setParallelism(int parallelism) {
         this.parallelism = parallelism;
+    }
+
+    public int getNumTasks() {
+        return numTasks;
+    }
+
+    public void setNumTasks(int numTasks) {
+        this.numTasks = numTasks;
+    }
+
+    public int getOnHeapMemoryLoad() {
+        return onHeapMemoryLoad;
+    }
+
+    public void setOnHeapMemoryLoad(int onHeapMemoryLoad) {
+        this.onHeapMemoryLoad = onHeapMemoryLoad;
+    }
+
+    public int getOffHeapMemoryLoad() {
+        return offHeapMemoryLoad;
+    }
+
+    public void setOffHeapMemoryLoad(int offHeapMemoryLoad) {
+        this.offHeapMemoryLoad = offHeapMemoryLoad;
+    }
+
+    public int getCpuLoad() {
+        return cpuLoad;
+    }
+
+    public void setCpuLoad(int cpuLoad) {
+        this.cpuLoad = cpuLoad;
     }
 }
