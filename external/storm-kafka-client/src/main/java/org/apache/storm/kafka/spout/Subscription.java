c7b7b896bb0101a1b1b56677b647550b615d515a
Stig Rohde Døssing <stigdoessing@gmail.com>
2017-06-05 14:59:19 +0200

    STORM-2541: Fix storm-kafka-client manual subscription not being able to start consuming

--- external/storm-kafka-client/src/main/java/org/apache/storm/kafka/spout/Subscription.java
+++ external/storm-kafka-client/src/main/java/org/apache/storm/kafka/spout/Subscription.java
@@ -37,7 +37,7 @@ public abstract class Subscription implements Serializable {
     public abstract <K, V> void subscribe(KafkaConsumer<K,V> consumer, ConsumerRebalanceListener listener, TopologyContext context);
     
     /**
-     * @return a string representing the subscribed topics.
+     * @return A human-readable string representing the subscribed topics.
      */
     public abstract String getTopicsString();
     
