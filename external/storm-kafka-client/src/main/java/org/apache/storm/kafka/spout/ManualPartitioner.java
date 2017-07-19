fde1b81834c9d4e2d01c7412db66b422d2d3db84
Stig Rohde Døssing <srdo@apache.org>
2017-07-19 15:25:18 +0200

    STORM-2642: Storm-kafka-client spout cannot be serialized when using manual partition assignment

--- external/storm-kafka-client/src/main/java/org/apache/storm/kafka/spout/ManualPartitioner.java
+++ external/storm-kafka-client/src/main/java/org/apache/storm/kafka/spout/ManualPartitioner.java
@@ -19,6 +19,7 @@ package org.apache.storm.kafka.spout;
 
 import java.util.List;
 
+import java.io.Serializable;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.storm.task.TopologyContext;
 
@@ -28,7 +29,7 @@ import org.apache.storm.task.TopologyContext;
  * The complete TopologyContext is passed in, but it is suggested that you use the index of the spout and the total
  * number of spouts to avoid missing partitions or double assigning partitions.
  */
-public interface ManualPartitioner {
+public interface ManualPartitioner extends Serializable {
     /**
      * Get the partitions for this assignment
      * @param allPartitions all of the partitions that the set of spouts want to subscribe to, in a strict ordering
