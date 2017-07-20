37f9c59949a1d8873668934b317e5c1ba20d3599
Jungtaek Lim <kabhwan@gmail.com>
2017-07-20 19:51:17 +0900

    STORM-1280 port backtype.storm.daemon.logviewer to java
    * apply changed API path to integration-test
      * didn't realize integration accesses logviewer

--- integration-test/src/test/java/org/apache/storm/st/wrapper/TopoWrap.java
+++ integration-test/src/test/java/org/apache/storm/st/wrapper/TopoWrap.java
@@ -261,8 +261,8 @@ public class TopoWrap {
 
         public ExecutorURL(String componentId, String host, int logViewerPort, int executorPort, String topoId) throws MalformedURLException {
             String sep = "%2F"; //hex of "/"
-            String viewUrlStr = String.format("http://%s:%s/log?file=", host, logViewerPort);
-            String downloadUrlStr = String.format("http://%s:%s/download?file=%%2F", host, logViewerPort);
+            String viewUrlStr = String.format("http://%s:%s/api/v1/log?file=", host, logViewerPort);
+            String downloadUrlStr = String.format("http://%s:%s/api/v1/download?file=%%2F", host, logViewerPort);
             viewUrl = new URL(String.format("%s/%s%s%d%sworker.log", viewUrlStr, topoId, sep, executorPort, sep));
             downloadUrl = new URL(String.format("%s/%s%s%d%sworker.log", downloadUrlStr, topoId, sep, executorPort, sep));
             this.componentId = componentId;
