f3a400117f70dfd437a39f025cfd1d7fe39eb96a
Kyle Nusbaum <knusbaum@yahoo-inc.com>
2017-08-09 17:06:33 -0500

    Fixing tests.

--- storm-client/test/jvm/org/apache/storm/utils/UtilsTest.java
+++ storm-client/test/jvm/org/apache/storm/utils/UtilsTest.java
@@ -22,8 +22,6 @@ import org.apache.storm.Config;
 import org.apache.thrift.transport.TTransportException;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -174,38 +172,5 @@ public class UtilsTest {
                 System.setProperty(key, oldValue);
             }
         }
-    }
-
-    @Rule
-    public final ExpectedException schedulerException = ExpectedException.none();
-
-    @Test
-    public void testSchedulerStrategyWhitelist() {
-        Map<String, Object> config = ConfigUtils.readStormConfig();
-        String allowed = "org.apache.storm.scheduler.resource.strategies.scheduling.DefaultResourceAwareStrategy";
-        config.put(Config.NIMBUS_SCHEDULER_STRATEGY_CLASS_WHITELIST, Arrays.asList(allowed));
-
-        Object sched = ReflectionUtils.newSchedulerStrategyInstance(allowed, config);
-        Assert.assertEquals(sched.getClass().getName(), allowed);
-    }
-
-    @Test
-    public void testSchedulerStrategyWhitelistException() {
-        Map<String, Object> config = ConfigUtils.readStormConfig();
-        String allowed = "org.apache.storm.scheduler.resource.strategies.scheduling.SomeNonExistantStrategy";
-        String notAllowed = "org.apache.storm.scheduler.resource.strategies.scheduling.DefaultResourceAwareStrategy";
-        config.put(Config.NIMBUS_SCHEDULER_STRATEGY_CLASS_WHITELIST, Arrays.asList(allowed));
-
-        schedulerException.expect(DisallowedStrategyException.class);
-        ReflectionUtils.newSchedulerStrategyInstance(notAllowed, config);
-    }
-
-    @Test
-    public void testSchedulerStrategyEmptyWhitelist() {
-        Map<String, Object> config = ConfigUtils.readStormConfig();
-        String allowed = "org.apache.storm.scheduler.resource.strategies.scheduling.DefaultResourceAwareStrategy";
-
-        Object sched = ReflectionUtils.newSchedulerStrategyInstance(allowed, config);
-        Assert.assertEquals(sched.getClass().getName(), allowed);
     }
 }
