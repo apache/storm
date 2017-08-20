fe1c44aa9d8acbab66c83e8a9c8e9e68e12b385c
kamleshbhatt <kbhatt2005@gmail.com>
2017-08-20 23:13:39 +0530

    minor fixes

--- storm-client/src/jvm/org/apache/storm/streams/PairStream.java
+++ storm-client/src/jvm/org/apache/storm/streams/PairStream.java
@@ -383,7 +383,8 @@ public class PairStream<K, V> extends Stream<Pair<K, V>> {
     }
 
     /**
-     * Groups the values of this stream with the values having the same key from the other stream.
+     * Groups the values of this stream with the values having the same key from
+     * the other stream.
      * If stream1 has values - (k1, v1), (k2, v2), (k2, v3)
      * and stream2 has values - (k1, x1), (k1, x2), (k3, x3)
      * The the co-grouped stream would contain -
