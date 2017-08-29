f9beb5358c91a799bec49f071a8f68153042a48a
Roshan Naik <roshan@HW13642.hsd1.ca.comcast.net>
2017-08-28 22:29:31 -0700

    Revert changes in ShuffleGrouping until there is consensus on thread safety in groupers.

--- storm-client/src/jvm/org/apache/storm/grouping/ShuffleGrouping.java
+++ storm-client/src/jvm/org/apache/storm/grouping/ShuffleGrouping.java
@@ -26,11 +26,12 @@ import java.util.Random;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class ShuffleGrouping implements CustomStreamGrouping, Serializable {
     private ArrayList<List<Integer>> choices;
-    private int current = 0 ;
+    private AtomicInteger current;
 
     @Override
     public void prepare(WorkerTopologyContext context, GlobalStreamId stream, List<Integer> targetTasks) {
@@ -38,14 +39,22 @@ public class ShuffleGrouping implements CustomStreamGrouping, Serializable {
         for (Integer i: targetTasks) {
             choices.add(Arrays.asList(i));
         }
+        current = new AtomicInteger(0);
         Collections.shuffle(choices, new Random());
     }
 
     @Override
-    public synchronized List<Integer> chooseTasks(int taskId, List<Object> values) {
-        if (current==choices.size()) {
-            current = 0;
-	}
-        return choices.get(current++);
+    public List<Integer> chooseTasks(int taskId, List<Object> values) {
+        int rightNow;
+        int size = choices.size();
+        while (true) {
+            rightNow = current.incrementAndGet();
+            if (rightNow < size) {
+                return choices.get(rightNow);
+            } else if (rightNow == size) {
+                current.set(0);
+                return choices.get(0);
+            }
+        } // race condition with another thread, and we lost. try again
     }
 }
