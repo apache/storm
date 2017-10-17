e69fbc51ada2fd542d717bbd7623c39f4f6a1cb8
Jungtaek Lim <kabhwan@gmail.com>
2017-10-17 12:40:48 +0900

    STORM-2779 NPE on shutting down WindowedBoltExecutor
    * waterMarkEventGenerator could be null when timestamp field is not specified

--- storm-client/src/jvm/org/apache/storm/topology/WindowedBoltExecutor.java
+++ storm-client/src/jvm/org/apache/storm/topology/WindowedBoltExecutor.java
@@ -327,7 +327,9 @@ public class WindowedBoltExecutor implements IRichBolt {
 
     @Override
     public void cleanup() {
-        waterMarkEventGenerator.shutdown();
+        if (waterMarkEventGenerator != null) {
+            waterMarkEventGenerator.shutdown();
+        }
         windowManager.shutdown();
         bolt.cleanup();
     }
