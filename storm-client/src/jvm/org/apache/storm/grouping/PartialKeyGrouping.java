d884f8a3dda9f9d81064280e61eb9e56d4542e87
Kevin Peek <kpeek@salesforce.com>
2017-10-30 11:22:20 -0400

    STORM-2782 - refactor partial key grouping

--- storm-client/src/jvm/org/apache/storm/grouping/PartialKeyGrouping.java
+++ storm-client/src/jvm/org/apache/storm/grouping/PartialKeyGrouping.java
@@ -17,40 +17,64 @@
  */
 package org.apache.storm.grouping;
 
+import com.google.common.collect.Maps;
+
 import java.io.Serializable;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import org.apache.storm.generated.GlobalStreamId;
 import org.apache.storm.task.WorkerTopologyContext;
 import org.apache.storm.tuple.Fields;
 
-import com.google.common.hash.HashFunction;
-import com.google.common.hash.Hashing;
 
+/**
+ * A variation on FieldGrouping. This grouping operates on a partitioning of the incoming
+ * tuples (like a FieldGrouping), but it can send Tuples from a given partition to
+ * multiple downstream tasks.
+ *
+ * Given a total pool of target tasks, this grouping will always send Tuples with a given
+ * key to one member of a subset of those tasks. Each key is assigned a subset of tasks.
+ * Each tuple is then sent to one task from that subset.
+ *
+ * Notes:
+ * - the default TaskSelector ensures each task gets as close to a balanced number of Tuples as possible
+ * - the default AssignmentCreator hashes the key and produces an assignment of two tasks
+ */
 public class PartialKeyGrouping implements CustomStreamGrouping, Serializable {
-    private static final long serialVersionUID = -447379837314000353L;
+    private static final long serialVersionUID = -1672360572274911808L;
     private List<Integer> targetTasks;
-    private long[] targetTaskStats;
-    private HashFunction h1 = Hashing.murmur3_128(13);
-    private HashFunction h2 = Hashing.murmur3_128(17);
     private Fields fields = null;
     private Fields outFields = null;
 
+    private AssignmentCreator assignmentCreator;
+    private TargetSelector targetSelector;
+
     public PartialKeyGrouping() {
-        //Empty
+        this(null);
     }
 
     public PartialKeyGrouping(Fields fields) {
+        this(fields, new RandomTwoTaskAssignmentCreator(), new BalancedTargetSelector());
+    }
+
+    public PartialKeyGrouping(Fields fields, AssignmentCreator assignmentCreator) {
+        this(fields, assignmentCreator, new BalancedTargetSelector());
+    }
+    
+    public PartialKeyGrouping(Fields fields, AssignmentCreator assignmentCreator, TargetSelector targetSelector) {
         this.fields = fields;
+        this.assignmentCreator = assignmentCreator;
+        this.targetSelector = targetSelector;
     }
 
     @Override
     public void prepare(WorkerTopologyContext context, GlobalStreamId stream, List<Integer> targetTasks) {
         this.targetTasks = targetTasks;
-        targetTaskStats = new long[this.targetTasks.size()];
         if (this.fields != null) {
             this.outFields = context.getComponentOutputFields(stream);
         }
@@ -60,47 +84,126 @@ public class PartialKeyGrouping implements CustomStreamGrouping, Serializable {
     public List<Integer> chooseTasks(int taskId, List<Object> values) {
         List<Integer> boltIds = new ArrayList<>(1);
         if (values.size() > 0) {
-            byte[] raw;
-            if (fields != null) {
-                List<Object> selectedFields = outFields.select(fields, values);
-                ByteBuffer out = ByteBuffer.allocate(selectedFields.size() * 4);
-                for (Object o: selectedFields) {
-                    if (o instanceof List) {
-                        out.putInt(Arrays.deepHashCode(((List)o).toArray()));
-                    } else if (o instanceof Object[]) {
-                        out.putInt(Arrays.deepHashCode((Object[])o));
-                    } else if (o instanceof byte[]) {
-                        out.putInt(Arrays.hashCode((byte[]) o));
-                    } else if (o instanceof short[]) {
-                        out.putInt(Arrays.hashCode((short[]) o));
-                    } else if (o instanceof int[]) {
-                        out.putInt(Arrays.hashCode((int[]) o));
-                    } else if (o instanceof long[]) {
-                        out.putInt(Arrays.hashCode((long[]) o));
-                    } else if (o instanceof char[]) {
-                        out.putInt(Arrays.hashCode((char[]) o));
-                    } else if (o instanceof float[]) {
-                        out.putInt(Arrays.hashCode((float[]) o));
-                    } else if (o instanceof double[]) {
-                        out.putInt(Arrays.hashCode((double[]) o));
-                    } else if (o instanceof boolean[]) {
-                        out.putInt(Arrays.hashCode((boolean[]) o));
-                    } else if (o != null) {
-                        out.putInt(o.hashCode());
-                    } else {
-                      out.putInt(0);
-                    }
-                }
-                raw = out.array();
-            } else {
-                raw = values.get(0).toString().getBytes(); // assume key is the first field
-            }
-            int firstChoice = (int) (Math.abs(h1.hashBytes(raw).asLong()) % this.targetTasks.size());
-            int secondChoice = (int) (Math.abs(h2.hashBytes(raw).asLong()) % this.targetTasks.size());
-            int selected = targetTaskStats[firstChoice] > targetTaskStats[secondChoice] ? secondChoice : firstChoice;
-            boltIds.add(targetTasks.get(selected));
-            targetTaskStats[selected]++;
+            final byte[] rawKeyBytes = getKeyBytes(values);
+
+            final int[] taskAssignmentForKey = assignmentCreator.createAssignment(this.targetTasks, rawKeyBytes);
+            final int selectedTask = targetSelector.chooseTask(taskAssignmentForKey);
+
+            boltIds.add(selectedTask);
         }
         return boltIds;
+    }
+
+
+    /**
+     * Extract the key from the input Tuple.
+     */
+    private byte[] getKeyBytes(List<Object> values) {
+        byte[] raw;
+        if (fields != null) {
+            List<Object> selectedFields = outFields.select(fields, values);
+            ByteBuffer out = ByteBuffer.allocate(selectedFields.size() * 4);
+            for (Object o: selectedFields) {
+                if (o instanceof List) {
+                    out.putInt(Arrays.deepHashCode(((List)o).toArray()));
+                } else if (o instanceof Object[]) {
+                    out.putInt(Arrays.deepHashCode((Object[])o));
+                } else if (o instanceof byte[]) {
+                    out.putInt(Arrays.hashCode((byte[]) o));
+                } else if (o instanceof short[]) {
+                    out.putInt(Arrays.hashCode((short[]) o));
+                } else if (o instanceof int[]) {
+                    out.putInt(Arrays.hashCode((int[]) o));
+                } else if (o instanceof long[]) {
+                    out.putInt(Arrays.hashCode((long[]) o));
+                } else if (o instanceof char[]) {
+                    out.putInt(Arrays.hashCode((char[]) o));
+                } else if (o instanceof float[]) {
+                    out.putInt(Arrays.hashCode((float[]) o));
+                } else if (o instanceof double[]) {
+                    out.putInt(Arrays.hashCode((double[]) o));
+                } else if (o instanceof boolean[]) {
+                    out.putInt(Arrays.hashCode((boolean[]) o));
+                } else if (o != null) {
+                    out.putInt(o.hashCode());
+                } else {
+                    out.putInt(0);
+                }
+            }
+            raw = out.array();
+        } else {
+            raw = values.get(0).toString().getBytes(); // assume key is the first field
+        }
+        return raw;
+    }
+
+    /*==================================================
+     * Helper Classes
+     *==================================================*/
+
+    /**
+     * This interface is responsible for choosing a subset of the target tasks to use for a given key.
+     *
+     * NOTE: whatever scheme you use to create the assignment should be deterministic. This may be executed on multiple
+     * Storm Workers, thus each of them needs to come up with the same assignment for a given key.
+     */
+    public interface AssignmentCreator extends Serializable {
+        int[] createAssignment(List<Integer> targetTasks, byte[] key);
+    }
+
+    /**
+     * This interface chooses one element from a task assignment to send a specific Tuple to.
+     */
+    public interface TargetSelector extends Serializable {
+        Integer chooseTask(int[] assignedTasks);
+    }
+
+    /*========== Implementations ==========*/
+
+    /**
+     * This implementation of AssignmentCreator chooses two arbitrary tasks.
+     */
+    public static class RandomTwoTaskAssignmentCreator implements AssignmentCreator {
+        /**
+         * Creates a two task assignment by selecting random tasks.
+         */
+        public int[] createAssignment(List<Integer> tasks, byte[] key) {
+            // It is necessary that this produce a deterministic assignment based on the key, so seed the Random from the key
+            final long seedForRandom = Arrays.hashCode(key);
+            final Random random = new Random(seedForRandom);
+            final int choice1 = random.nextInt(tasks.size());
+            int choice2 = random.nextInt(tasks.size());
+            // ensure that choice1 and choice2 are not the same task
+            choice2 = choice1 == choice2 ? (choice2 + 1) % tasks.size() : choice2;
+            return new int[] {tasks.get(choice1), tasks.get(choice2)};
+        }
+    }
+
+    /**
+     * A basic implementation of target selection. This strategy chooses the task within the assignment that has
+     * received the fewest Tuples overall from this instance of the grouping.
+     */
+    public static class BalancedTargetSelector implements TargetSelector {
+        private Map<Integer, Long> targetTaskStats = Maps.newHashMap();
+
+        /**
+         * Chooses one of the incoming tasks and selects the one that has been selected
+         * the fewest times so far.
+         */
+        public Integer chooseTask(int[] assignedTasks) {
+            Integer taskIdWithMinLoad = null;
+            Long minTaskLoad = Long.MAX_VALUE;
+
+            for (Integer currentTaskId : assignedTasks) {
+                final Long currentTaskLoad = targetTaskStats.getOrDefault(currentTaskId, 0L);
+                if (currentTaskLoad < minTaskLoad) {
+                    minTaskLoad = currentTaskLoad;
+                    taskIdWithMinLoad = currentTaskId;
+                }
+            }
+
+            targetTaskStats.put(taskIdWithMinLoad, targetTaskStats.getOrDefault(taskIdWithMinLoad, 0L) + 1);
+            return taskIdWithMinLoad;
+        }
     }
 }
