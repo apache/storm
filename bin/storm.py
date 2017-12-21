032970bae1b40d17ecc06282b44e63611c11ab33
Arun Mahadevan <arunm@apache.org>
2017-12-20 16:28:26 -0800

    Revert "Merge branch 'STORM-2597' of https://github.com/revans2/incubator-storm into STORM-2597"
    This reverts commit 14980968c4424cc57aa2af627b3b0c4afca0e734.

--- bin/storm.py
+++ bin/storm.py
@@ -148,9 +148,11 @@ def get_classpath(extrajars, daemon=True):
     if daemon:
         ret.extend(get_wildcard_dir(STORM_DIR + "/extlib-daemon"))
     if STORM_EXT_CLASSPATH != None:
-        ret.append(STORM_EXT_CLASSPATH)
+        for path in STORM_EXT_CLASSPATH.split(os.pathsep):
+            ret.extend(get_wildcard_dir(path))
     if daemon and STORM_EXT_CLASSPATH_DAEMON != None:
-        ret.append(STORM_EXT_CLASSPATH_DAEMON)
+        for path in STORM_EXT_CLASSPATH_DAEMON.split(os.pathsep):
+            ret.extend(get_wildcard_dir(path))
     ret.extend(extrajars)
     return normclasspath(os.pathsep.join(ret))
 
