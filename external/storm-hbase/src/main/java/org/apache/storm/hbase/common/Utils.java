434e0bb6af9a694c14d8a6bdf7415531f6fed6bf
Jungtaek Lim <kabhwan@gmail.com>
2018-01-23 13:14:13 +0900

    BUG-94859 HDFS and Hbase bolt fails with GSS no valid credentials exception
    * find HBase delegation token in tokens and use it as ugi
    
    Change-Id: I221c03f895e50fbfa40c50c3a4329adcff852d3a

--- external/storm-hbase/src/main/java/org/apache/storm/hbase/common/Utils.java
+++ external/storm-hbase/src/main/java/org/apache/storm/hbase/common/Utils.java
@@ -34,6 +34,7 @@ import java.security.PrivilegedExceptionAction;
 
 public class Utils {
     private static final Logger LOG = LoggerFactory.getLogger(Utils.class);
+    public static final String TOKEN_KIND_HBASE_AUTH_TOKEN = "HBASE_AUTH_TOKEN";
 
     private Utils(){}
 
@@ -48,13 +49,26 @@ public class Utils {
             ugi = UserGroupInformation.getCurrentUser();
 
             LOG.debug("UGI for current USER : {}", ugi.getUserName());
+            boolean foundHBaseAuthToken = false;
             for (Token<? extends TokenIdentifier> token : ugi.getTokens()) {
                 LOG.debug("Token in UGI (delegation token): {} / {}", token.toString(),
                         token.decodeIdentifier().getUser());
 
-                // use UGI from token
-                ugi = token.decodeIdentifier().getUser();
-                ugi.addToken(token);
+                // token.getKind() = Text, Text is annotated by @Stringable
+                // which ensures toString() implementation
+                if (token.getKind().toString().equals(TOKEN_KIND_HBASE_AUTH_TOKEN)) {
+                    // use UGI from token
+                    LOG.debug("Found HBASE_AUTH_TOKEN - using the token to replace current user.");
+
+                    ugi = token.decodeIdentifier().getUser();
+                    ugi.addToken(token);
+
+                    foundHBaseAuthToken = true;
+                }
+            }
+
+            if (!foundHBaseAuthToken) {
+                LOG.warn("Can't find HBase auth token in delegation tokens.");
             }
         }
 
