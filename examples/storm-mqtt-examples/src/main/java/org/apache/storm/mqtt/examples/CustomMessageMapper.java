a344a29b9ce4bd71259452e7a9cb5b3284335ae2
Karl-Philipp Richter <krichter722@aol.de>
2018-03-19 02:10:34 +0100

    [STORM-3007] storm-mqtt-examples: fixed all checkstyle warnings

--- examples/storm-mqtt-examples/src/main/java/org/apache/storm/mqtt/examples/CustomMessageMapper.java
+++ examples/storm-mqtt-examples/src/main/java/org/apache/storm/mqtt/examples/CustomMessageMapper.java
@@ -15,35 +15,65 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.storm.mqtt.examples;
 
-import org.apache.storm.tuple.Fields;
-import org.apache.storm.tuple.Values;
 import org.apache.storm.mqtt.MqttMessage;
 import org.apache.storm.mqtt.MqttMessageMapper;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Values;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Given a topic name: "users/{user}/{location}/{deviceId}"
  * and a payload of "{temperature}/{humidity}"
- * emits a tuple containing user(String), deviceId(String), location(String), temperature(float), humidity(float)
- *
+ * emits a tuple containing
+ * {@code user(String), deviceId(String), location(String), temperature(float),
+ * humidity(float)}.
  */
-public class CustomMessageMapper implements MqttMessageMapper {
-    private static final Logger LOG = LoggerFactory.getLogger(CustomMessageMapper.class);
+public final class CustomMessageMapper implements MqttMessageMapper {
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOG = LoggerFactory.getLogger(
+            CustomMessageMapper.class);
+    private static final int TOPIC_INDEX_1 = 2;
+    private static final int TOPIC_INDEX_2 = 4;
+    private static final int TOPIC_INDEX_3 = 3;
 
-
-    public Values toValues(MqttMessage message) {
+    /**
+     * Converts MQTT message to an instance of {@code Values}.
+     * @param message the message to convert
+     * @return the converted values
+     */
+    @Override
+    public Values toValues(final MqttMessage message) {
         String topic = message.getTopic();
         String[] topicElements = topic.split("/");
         String[] payloadElements = new String(message.getMessage()).split("/");
 
-        return new Values(topicElements[2], topicElements[4], topicElements[3], Float.parseFloat(payloadElements[0]),
+        return new Values(topicElements[TOPIC_INDEX_1],
+                topicElements[TOPIC_INDEX_2],
+                topicElements[TOPIC_INDEX_3],
+                Float.parseFloat(payloadElements[0]),
                 Float.parseFloat(payloadElements[1]));
     }
 
+    /**
+     * Gets the output fields.
+     * @return the output fields
+     */
+    @Override
     public Fields outputFields() {
-        return new Fields("user", "deviceId", "location", "temperature", "humidity");
+        return new Fields("user",
+                "deviceId",
+                "location",
+                "temperature",
+                "humidity");
+    }
+
+    /**
+     * Utility constructor.
+     */
+    private CustomMessageMapper() {
     }
 }
