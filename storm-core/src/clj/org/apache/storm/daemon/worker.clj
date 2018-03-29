6a0defee872abba9e12fcc125960bfd931d7f278
mhummel <hummel.michel@gmail.com>
2018-03-29 18:00:23 +0200

    use a duplicated ByteBuffer instead of resetting position

--- storm-core/src/clj/org/apache/storm/daemon/worker.clj
+++ storm-core/src/clj/org/apache/storm/daemon/worker.clj
@@ -566,12 +566,10 @@
         worker-topology-context (worker-context worker)
         hooks (.get_worker_hooks topology)]
     (dofor [hook hooks]
-      (let [savedposition (.position hook)
-            hook-bytes (Utils/toByteArray hook)
-            deser-hook (Utils/javaDeserialize hook-bytes BaseWorkerHook)
-            nop (.position hook savedposition)]
-        (.start deser-hook topo-conf worker-topology-context))
-      )))
+      (let [duplicated-hook (.duplicate hook)
+            hook-bytes (Utils/toByteArray duplicated-hook)
+            deser-hook (Utils/javaDeserialize hook-bytes BaseWorkerHook)]
+        (.start deser-hook topo-conf worker-topology-context)))))
 
 (defn run-worker-shutdown-hooks [worker]
   (let [topology (:topology worker)
