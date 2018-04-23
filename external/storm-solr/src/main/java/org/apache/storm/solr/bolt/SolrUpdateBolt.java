53adebcfec5dec5e7a59f0a9108da734d3a3f01a
Kishor Patil <kpatil@yahoo-inc.com>
2018-04-22 23:24:58 -0400

    Fixing stylecheck problems with storm-solr

--- external/storm-solr/src/main/java/org/apache/storm/solr/bolt/SolrUpdateBolt.java
+++ external/storm-solr/src/main/java/org/apache/storm/solr/bolt/SolrUpdateBolt.java
@@ -1,19 +1,13 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The ASF licenses this file to you under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
  */
 
 package org.apache.storm.solr.bolt;
@@ -22,7 +16,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrRequest;
 import org.apache.solr.client.solrj.SolrServerException;
@@ -68,15 +61,16 @@ public class SolrUpdateBolt extends BaseTickTupleAwareRichBolt {
         this.solrMapper = solrMapper;
         this.commitStgy = commitStgy;
         LOG.debug("Created {} with the following configuration: " +
-                    "[SolrConfig = {}], [SolrMapper = {}], [CommitStgy = {}]",
-                    this.getClass().getSimpleName(), solrConfig, solrMapper, commitStgy);
+                  "[SolrConfig = {}], [SolrMapper = {}], [CommitStgy = {}]",
+                  this.getClass().getSimpleName(), solrConfig, solrMapper, commitStgy);
     }
 
     @Override
     public void prepare(Map<String, Object> topoConf, TopologyContext context, OutputCollector collector) {
         this.collector = collector;
-        if (solrConfig.isKerberosEnabled())
+        if (solrConfig.isKerberosEnabled()) {
             HttpClientUtil.setConfigurer(new Krb5HttpClientConfigurer());
+        }
         solrMapper.configure();
         this.solrClient = new CloudSolrClient(solrConfig.getZkHostString());
         this.toCommitTuples = new ArrayList<>(capacity());
@@ -85,8 +79,8 @@ public class SolrUpdateBolt extends BaseTickTupleAwareRichBolt {
     private int capacity() {
         final int defArrListCpcty = 10;
         return (commitStgy instanceof CountBasedCommit) ?
-                ((CountBasedCommit)commitStgy).getThreshold() :
-                defArrListCpcty;
+            ((CountBasedCommit) commitStgy).getThreshold() :
+            defArrListCpcty;
     }
 
     @Override
--- external/storm-solr/src/main/java/org/apache/storm/solr/schema/Schema.java
+++ external/storm-solr/src/main/java/org/apache/storm/solr/schema/Schema.java
@@ -1,19 +1,13 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The ASF licenses this file to you under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
  */
 
 package org.apache.storm.solr.schema;
@@ -40,52 +34,52 @@ public class Schema implements Serializable {
         return name;
     }
 
-    public String getVersion() {
-        return version;
-    }
-
-    public String getUniqueKey() {
-        return uniqueKey;
-    }
-
-    public List<FieldType> getFieldTypes() {
-        return Collections.unmodifiableList(fieldTypes);
-    }
-
-    public List<Field> getFields() {
-        return Collections.unmodifiableList(fields);
-    }
-
-    public List<Field> getDynamicFields() {
-        return Collections.unmodifiableList(dynamicFields);
-    }
-
-    public List<CopyField> getCopyFields() {
-        return Collections.unmodifiableList(copyFields);
-    }
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
     }
 
     public void setVersion(String version) {
         this.version = version;
     }
 
+    public String getUniqueKey() {
+        return uniqueKey;
+    }
+
     public void setUniqueKey(String uniqueKey) {
         this.uniqueKey = uniqueKey;
+    }
+
+    public List<FieldType> getFieldTypes() {
+        return Collections.unmodifiableList(fieldTypes);
     }
 
     public void setFieldTypes(List<FieldType> fieldTypes) {
         this.fieldTypes = fieldTypes;
     }
 
+    public List<Field> getFields() {
+        return Collections.unmodifiableList(fields);
+    }
+
     public void setFields(List<Field> fields) {
         this.fields = fields;
     }
 
+    public List<Field> getDynamicFields() {
+        return Collections.unmodifiableList(dynamicFields);
+    }
+
     public void setDynamicFields(List<Field> dynamicFields) {
         this.dynamicFields = dynamicFields;
+    }
+
+    public List<CopyField> getCopyFields() {
+        return Collections.unmodifiableList(copyFields);
     }
 
     public void setCopyFields(List<CopyField> copyFields) {
@@ -95,14 +89,14 @@ public class Schema implements Serializable {
     @Override
     public String toString() {
         return "Schema{" +
-                "name='" + name + '\'' +
-                ", version='" + version + '\'' +
-                ", uniqueKey='" + uniqueKey + '\'' +
-                ", fieldTypes=" + fieldTypes +
-                ", fields=" + fields +
-                ", dynamicFields=" + dynamicFields +
-                ", copyFields=" + copyFields +
-                '}';
+               "name='" + name + '\'' +
+               ", version='" + version + '\'' +
+               ", uniqueKey='" + uniqueKey + '\'' +
+               ", fieldTypes=" + fieldTypes +
+               ", fields=" + fields +
+               ", dynamicFields=" + dynamicFields +
+               ", copyFields=" + copyFields +
+               '}';
     }
 
     // Wrapper class handy for the client code to use the JSON parser to build to use with JSON parser
