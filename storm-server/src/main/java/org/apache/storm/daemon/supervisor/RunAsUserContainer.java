82deb62dd3a6f58e5569244517bc93647fcab2a2
Arun Mahadevan <arunm@apache.org>
2018-06-15 16:32:53 -0700

    STORM-3110: Skip the user while checking isProcessAlive
    * reintroduce user check and rely on 'user.name' when not running worker as launching user

--- storm-server/src/main/java/org/apache/storm/daemon/supervisor/RunAsUserContainer.java
+++ storm-server/src/main/java/org/apache/storm/daemon/supervisor/RunAsUserContainer.java
@@ -102,4 +102,16 @@ public class RunAsUserContainer extends BasicContainer {
         }
         ClientSupervisorUtils.processLauncher(_conf, user, commandPrefix, args, null, logPrefix, processExitCallback, targetDir);
     }
+
+    /**
+     * If 'supervisor.run.worker.as.user' is set, worker will be launched as the user that launched the topology.
+     */
+    @Override
+    protected String getRunWorkerAsUser() {
+        try {
+            return getWorkerUser();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 }
