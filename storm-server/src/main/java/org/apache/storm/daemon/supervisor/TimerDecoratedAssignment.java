cd7f239a50a8e0e30795d9a750ff23f189a58ec7
Zhengdai Hu <hu.zhengdai@gmail.com>
2018-06-27 14:46:33 -0500

    STORM-3130: Added object wrapper with timer in both decorator and inheritance patterns.

---
+++ storm-server/src/main/java/org/apache/storm/daemon/supervisor/TimerDecoratedAssignment.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package org.apache.storm.daemon.supervisor;
+
+import com.codahale.metrics.Timer;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.apache.storm.generated.ExecutorInfo;
+import org.apache.storm.generated.LocalAssignment;
+import org.apache.storm.metric.timed.TimerDecorated;
+
+public class TimerDecoratedAssignment extends LocalAssignment implements TimerDecorated {
+    //TODO: Does this have to volatile?
+    private final AtomicReference<Timer.Context> timingRef = new AtomicReference<>();
+
+    public TimerDecoratedAssignment(Timer.Context timing) {
+        this.timingRef.set(timing);
+    }
+
+    public TimerDecoratedAssignment(String topology_id, List<ExecutorInfo> executors, Timer.Context timing) {
+        super(topology_id, executors);
+        this.timingRef.set(timing);
+    }
+
+    public TimerDecoratedAssignment(LocalAssignment other, Timer.Context timing) {
+        super(other);
+        this.timingRef.set(timing);
+    }
+
+    @Override
+    public boolean hasStopped() {
+        return hasStopped(timingRef);
+    }
+
+    @Override
+    public long stopTiming() {
+        return stopTiming(timingRef);
+    }
+}
---
+++ storm-server/src/main/java/org/apache/storm/metric/timed/TimerDecorated.java
@@ -0,0 +1,48 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package org.apache.storm.metric.timed;
+
+import com.codahale.metrics.Timer;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public interface TimerDecorated extends AutoCloseable {
+    boolean hasStopped();
+
+    default boolean hasStopped(final AtomicReference<Timer.Context> timing) {
+        return timing.get() == null;
+    }
+
+    long stopTiming();
+
+    /**
+     * Stop the timer for measured object. Should be executed only once.
+     *
+     * @return Time a object is in use, or under measurement, in nanoseconds.
+     * @throws NullPointerException this method is called more than once.
+     */
+    default long stopTiming(final AtomicReference<Timer.Context> timingRef) throws NullPointerException {
+        Timer.Context timing = timingRef.getAndSet(null);
+        if (timing != null) {
+            return timing.stop();
+        }
+        throw new NullPointerException("Timer for this object has stopped and cleared");
+    }
+
+    @Override
+    default void close() throws Exception {
+        stopTiming();
+    }
+}
