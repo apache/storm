c18e3e3b7aea86ec89d45e1a4c21282f58b3d09c
Stig Rohde Døssing <srdo@apache.org>
2019-08-27 22:28:50 +0200

    STORM-3500: Fix spelling error in property storm.blobstore.dependency.jar.upload.chuck.size.bytes

--- storm-client/src/jvm/org/apache/storm/Config.java
+++ storm-client/src/jvm/org/apache/storm/Config.java
@@ -1412,12 +1412,12 @@ public class Config extends HashMap<String, Object> {
     @IsInteger
     public static final String STORM_BLOBSTORE_INPUTSTREAM_BUFFER_SIZE_BYTES = "storm.blobstore.inputstream.buffer.size.bytes";
     /**
-     * What chuck size to use for storm client to upload dependency jars.
+     * What chunk size to use for storm client to upload dependency jars.
      */
     @IsPositiveNumber
     @IsInteger
-    public static final String STORM_BLOBSTORE_DEPENDENCY_JAR_UPLOAD_CHUCK_SIZE_BYTES =
-            "storm.blobstore.dependency.jar.upload.chuck.size.bytes";
+    public static final String STORM_BLOBSTORE_DEPENDENCY_JAR_UPLOAD_CHUNK_SIZE_BYTES =
+            "storm.blobstore.dependency.jar.upload.chunk.size.bytes";
     /**
      * FQCN of a class that implements {@code ISubmitterHook} @see ISubmitterHook for details.
      */
