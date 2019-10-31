4c69b96307bf5f0464cab6c5333171dadf939c97
Meng Li (Ethan) <ethanopensource@gmail.com>
2019-10-31 11:03:22 -0500

    MINOR: create a checklist for testing a new Storm release (#3161)
    * create a checklist for testing a new Storm release
    
    * add link to dev-tools/rc and remove MD5 file check

--- dev-tools/rc/verify-release-file.sh
+++ dev-tools/rc/verify-release-file.sh
@@ -34,21 +34,6 @@ else
   echo 'Signature seems not correct'
 fi
 
-# checking MD5
-GPG_MD5_FILE="/tmp/${TARGET_FILE}_GPG.md5"
-gpg --print-md MD5 ${TARGET_FILE} > ${GPG_MD5_FILE}
-MD5_TARGET_FILE="${TARGET_FILE}.md5"
-
-echo ">> checking MD5 file... (${MD5_TARGET_FILE})"
-diff ${GPG_MD5_FILE} ${MD5_TARGET_FILE}
-
-if [ $? -eq 0 ];
-then
-  echo 'MD5 file is correct'
-else
-  echo 'MD5 file is not correct'
-fi
-
 # checking SHA
 GPG_SHA_FILE="/tmp/${TARGET_FILE}_GPG.sha512"
 gpg --print-md SHA512 ${TARGET_FILE} > ${GPG_SHA_FILE}
