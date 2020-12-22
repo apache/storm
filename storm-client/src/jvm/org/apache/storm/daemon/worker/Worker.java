f50585ee79d96b0636fb44194b1c3262a620527f
Meng (Ethan) Li <ethanopensource@gmail.com>
2020-12-21 23:10:30 -0600

    [STORM-3728] Fix Worker LoginException issue with pacemaker when pacemaker.auth.method is KERBEROS

--- storm-client/src/jvm/org/apache/storm/daemon/worker/Worker.java
+++ storm-client/src/jvm/org/apache/storm/daemon/worker/Worker.java
@@ -121,6 +121,10 @@ public class Worker implements Shutdownable, DaemonCommon {
 
         this.topologyConf = ConfigUtils.overrideLoginConfigWithSystemProperty(ConfigUtils.readSupervisorStormConf(conf, topologyId));
 
+        // see STORM-3728.
+        topologyConf.put(Config.PACEMAKER_AUTH_METHOD, "NONE");
+        conf.put(Config.PACEMAKER_AUTH_METHOD, "NONE");
+
         if (supervisorIfaceSupplier == null) {
             this.supervisorIfaceSupplier = () -> {
                 try {
