e21b71b56353cc4c7ce6d628d44223e3ca32fe80
Bipin Prasad <bipin_prasad@yahoo.com>
2021-11-18 12:17:12 -0500

    [STORM-3807] Topology Stats columns show NaN (#3424)
    Co-authored-by: Bipin Prasad <bprasad@verizonmedia.com>

--- storm-webapp/src/main/java/org/apache/storm/daemon/ui/UIHelpers.java
+++ storm-webapp/src/main/java/org/apache/storm/daemon/ui/UIHelpers.java
@@ -1577,9 +1577,8 @@ public class UIHelpers {
             temp.put("emitted", emittedStatDisplayMap.get(window));
             temp.put("transferred", transferred.get(window));
             temp.put("completeLatency", StatsUtil.floatStr(completeLatency.get(getWindowHint(window))));
-            temp.put("acked", acked.get(window));
-            temp.put("failed", failed.get(window));
-
+            temp.put("acked", acked.getOrDefault(window, 0L));
+            temp.put("failed", failed.getOrDefault(window, 0L));
 
             result.add(temp);
         }
