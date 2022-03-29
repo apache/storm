c3382f39110ab2f7e78b5158f2a06cb55fca3fab
Aaron Gresch <agresch@yahoo-inc.com>
2022-03-29 14:59:27 -0500

    STORM-3838 prevent topology from setting storm.workers.artifacts.dir

--- storm-server/src/main/java/org/apache/storm/daemon/nimbus/Nimbus.java
+++ storm-server/src/main/java/org/apache/storm/daemon/nimbus/Nimbus.java
@@ -1133,6 +1133,10 @@ public class Nimbus implements Iface, Shutdownable, DaemonCommon {
      * @param topology  the Storm topology
      */
     private static Map<String, Object> normalizeConf(Map<String, Object> conf, Map<String, Object> topoConf, StormTopology topology) {
+
+        // clear any values from the topoConf that it should not be setting.
+        topoConf.remove(Config.STORM_WORKERS_ARTIFACTS_DIR);
+
         //ensure that serializations are same for all tasks no matter what's on
         // the supervisors. this also allows you to declare the serializations as a sequence
         List<Map<String, Object>> allConfs = new ArrayList<>();
