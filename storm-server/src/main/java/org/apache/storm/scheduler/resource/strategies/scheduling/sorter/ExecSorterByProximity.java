581a5680eeec83d9b39d171e9f7e281cb0bec067
bipinprasad <bipinprasad@apache.org>
2022-04-21 10:11:40 -0700

    [STORM-3854] Add @Override reuired by PMD in storm-server

--- storm-server/src/main/java/org/apache/storm/scheduler/resource/strategies/scheduling/sorter/ExecSorterByProximity.java
+++ storm-server/src/main/java/org/apache/storm/scheduler/resource/strategies/scheduling/sorter/ExecSorterByProximity.java
@@ -54,6 +54,7 @@ public class ExecSorterByProximity implements IExecSorter {
      * @param unassignedExecutors an unmodifiable set of executors that need to be scheduled.
      * @return a list of executors in sorted order for scheduling.
      */
+    @Override
     public List<ExecutorDetails> sortExecutors(Set<ExecutorDetails> unassignedExecutors) {
         Map<String, Component> componentMap = topologyDetails.getUserTopolgyComponents(); // excludes system components
         LinkedHashSet<ExecutorDetails> orderedExecutorSet = new LinkedHashSet<>(); // in insert order
