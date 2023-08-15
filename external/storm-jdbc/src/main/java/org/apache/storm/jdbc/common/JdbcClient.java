c961e24a121b43d4a1a776ed49409bc9e9f4c8b8
Richard Zowalla <richard.zowalla@hs-heilbronn.de>
2023-08-15 16:04:47 +0200

    STORM-3950 - Modernize storm-jdbc

--- external/storm-jdbc/src/main/java/org/apache/storm/jdbc/common/JdbcClient.java
+++ external/storm-jdbc/src/main/java/org/apache/storm/jdbc/common/JdbcClient.java
@@ -30,7 +30,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
