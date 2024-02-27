e61032fc1494b2769de35332805099f5b914370b
Richard Zowalla <richard.zowalla@hs-heilbronn.de>
2024-02-27 12:30:44 +0100

    STORM-4038 - Cleanup Hadoop/HDFS dependencies

--- external/storm-autocreds/src/main/java/org/apache/storm/hbase/security/AutoHBaseCommand.java
+++ external/storm-autocreds/src/main/java/org/apache/storm/hbase/security/AutoHBaseCommand.java
@@ -25,7 +25,6 @@ import java.util.HashMap;
 import java.util.Map;
 import javax.security.auth.Subject;
 
-import org.apache.storm.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
