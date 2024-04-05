874d0b08b9877cf3f7702c90ffd98e51347b055c
Sam Maclennan <4553618+sammac@users.noreply.github.com>
2024-04-05 13:28:09 -0400

    Simplify/Remove double delete/lookup in heartbeat cleanup code.

--- storm-client/src/jvm/org/apache/storm/utils/VersionedStore.java
+++ storm-client/src/jvm/org/apache/storm/utils/VersionedStore.java
@@ -128,15 +128,11 @@ public class VersionedStore {
     public void cleanup(int versionsToKeep) throws IOException {
         List<Long> versions = getAllVersions();
         if (versionsToKeep >= 0) {
-            versions = versions.subList(0, Math.min(versions.size(), versionsToKeep));
+            versions = versions.subList(Math.min(versions.size(), versionsToKeep), versions.size());
         }
-        HashSet<Long> keepers = new HashSet<Long>(versions);
 
-        for (String p : listDir(root)) {
-            Long v = parseVersion(p);
-            if (v != null && !keepers.contains(v)) {
-                deleteVersion(v);
-            }
+        for (Long v : versions) {
+            deleteVersion(v);
         }
     }
 
