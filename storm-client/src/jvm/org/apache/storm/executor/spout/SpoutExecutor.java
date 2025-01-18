677756f19ffdaea11d3cc18c7b618e137d59741c
reiabreu <rui.abreu@gmail.com>
2025-01-18 22:47:50 +0000

    Update SpoutExecutor.java

--- storm-client/src/jvm/org/apache/storm/executor/spout/SpoutExecutor.java
+++ storm-client/src/jvm/org/apache/storm/executor/spout/SpoutExecutor.java
@@ -17,7 +17,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicBoolean;
-
 import org.apache.storm.Config;
 import org.apache.storm.Constants;
 import org.apache.storm.ICredentialsListener;
