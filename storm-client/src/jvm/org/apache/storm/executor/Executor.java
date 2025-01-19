884c4cda1818d47730d99fa0e840028174cde422
reiabreu <rui.abreu@gmail.com>
2025-01-19 12:34:37 +0000

    StormExecutor: adding an unit test to changes introduced in STORM-3693  (#3834)
    * Porting changes from STORM-3693
    
    * Refactoring changes proposed on STORM-3693 and adding an unit test
    
    * Update Executor.java
    
    * Update SpoutExecutor.java
    
    * Update SpoutExecutor.java
    
    * Update SpoutExecutor.java
    
    * Adding missing license
    
    * Adding missing license

--- storm-client/src/jvm/org/apache/storm/executor/Executor.java
+++ storm-client/src/jvm/org/apache/storm/executor/Executor.java
@@ -2,9 +2,9 @@
  * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The ASF licenses this file to you under the Apache License, Version
  * 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
- * <p>
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
@@ -285,19 +285,17 @@ public abstract class Executor implements Callable, JCQueue.Consumer {
         int taskId = addressedTuple.getDest();
 
         TupleImpl tuple = (TupleImpl) addressedTuple.getTuple();
-        String streamId = tuple.getSourceStreamId();
-        boolean isSpout = this instanceof SpoutExecutor;
         if (isDebug) {
             LOG.info("Processing received TUPLE: {} for TASK: {} ", tuple, taskId);
         }
 
+        acceptTupleAction(taskId, tuple);
+    }
+
+    protected void acceptTupleAction(int taskId, TupleImpl tuple) {
         try {
             if (taskId != AddressedTuple.BROADCAST_DEST) {
                 tupleActionFn(taskId, tuple);
-            } else if (isSpout && streamId.equals(Constants.SYSTEM_TICK_STREAM_ID)) {
-                //taskId is irrelevant here. Ensures pending.rotate() is called once per tick.
-                tupleActionFn(taskIds.get(0), tuple);
-
             } else {
                 for (Integer t : taskIds) {
                     tupleActionFn(t, tuple);
