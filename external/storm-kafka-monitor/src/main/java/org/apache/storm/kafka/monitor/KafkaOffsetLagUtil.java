25675912451d6594470a2fa9507533804bcc1a18
Rui Abreu <rui.abreu@gmail.com>
2025-09-14 17:48:50 +0100

    Adapting code to Kafka 4 API

--- external/storm-kafka-monitor/src/main/java/org/apache/storm/kafka/monitor/KafkaOffsetLagUtil.java
+++ external/storm-kafka-monitor/src/main/java/org/apache/storm/kafka/monitor/KafkaOffsetLagUtil.java
@@ -20,6 +20,7 @@ package org.apache.storm.kafka.monitor;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
